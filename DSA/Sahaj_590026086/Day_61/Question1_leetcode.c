144bb5cc0c97e621e38badb5c1545f93d618dd25
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:37:42 +0530

    Create Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_61/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_61/Question1_leetcode.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int findJudge(int n, int** trust, int trustSize, int* trustColSize) {
     int* score = (int*)calloc(n + 1, sizeof(int));
     for (int i = 0; i < trustSize; i++) {
@@ -11,4 +12,12 @@ int findJudge(int n, int** trust, int trustSize, int* trustColSize) {
     }
     free(score);
     return -1;
+}
+
+int main() {
+    int t1[] = {1, 2};
+    int* trust[] = {t1};
+    int colSize = 2;
+    printf("%d\n", findJudge(2, trust, 1, &colSize));
+    return 0;
 }
