cf8c34b40cd3ccdc5ce9cadc8832847fd02cb358
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:24 +0530

    Create initial structure for Day 64 Question1_Leetcode.c

---
+++ DSA/Aarav_590023538/Day_64/Question1_Leetcode.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+#include <stdlib.h>
+
+// Range Sum of BST (LeetCode #938)
+TreeNode* buildTree(vector<int>& vals);
+int rangeSumBST(TreeNode* root, int low, int high);
