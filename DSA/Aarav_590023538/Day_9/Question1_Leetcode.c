4ac0ca5b5be30e9157edba4d8010fa05c7122117
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:24 +0530

    Implement solution logic for Day 9 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_9/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_9/Question1_Leetcode.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Length of Last Word (LeetCode #58)
-int lengthOfLastWord(char* s);
+int lengthOfLastWord(char* s) {
+    int i = 0;
+    int length = 0;
+
+    // Find the end of the string
+    while (s[i] != '\0') {
+        i++;
+    }
+
+    // Move backwards and skip trailing spaces
+    i--;
+    while (i >= 0 && s[i] == ' ') {
+        i--;
+    }
+
+    // Count characters of the last word
+    while (i >= 0 && s[i] != ' ') {
+        length++;
+        i--;
+    }
+
+    return length;
+}
