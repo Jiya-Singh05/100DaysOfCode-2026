e511eaf0d441d8c8068b538a165abdfcb504439e
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:08 +0530

    Implement solution logic for Day 57 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_57/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_57/Question1_Leetcode.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Sum of Left Leaves
-bool isLeaf(TreeNode* node);
-int sumOfLeftLeaves(TreeNode* root);
+pp
+class Solution {
+public:
+    bool isLeaf(TreeNode* node) {
+        return node && node->left == nullptr && node->right == nullptr;
+    }
+
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+
+        int sum = 0;
+
+        if (isLeaf(root->left))
+            sum += root->left->val;
+
+        sum += sumOfLeftLeaves(root->left);
+        sum += sumOfLeftLeaves(root->right);
+
+        return sum;
+    }
+};
