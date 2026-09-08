3bb0ed6803388a855e9497dd4e8ca58fb8dd74bb
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:41 +0530

    Create initial structure for Day 73 Question1_Leetcode.c

---
+++ DSA/Aarav_590023538/Day_73/Question1_Leetcode.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+#include <stdlib.h>
+
+// Find Closest Node to Given Two Nodes (LeetCode #2359)
+int closestMeetingNode(vector<int>& edges, int node1, int node2);
