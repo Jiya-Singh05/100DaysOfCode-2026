c48f293d98e60e490427d85eca6de908b2dd1362
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:25 +0530

    Create initial structure for Day 64 Question2.c

---
+++ DSA/Aarav_590023538/Day_64/Question2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+#include <stdlib.h>
+
+// The Conference Room
+TreeNode* build(vector<int>& tree, int idx);
+int lowestCommonAncestor(TreeNode* root, int a, int b);
