2ccf019db647ded9181c55b979b5384b7f476d31
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:39:25 +0530

    Implement solution logic for Day 80 Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_80/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_80/Question1_leetcode.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-
-// LeetCode 191: Number of 1 Bits
-int hammingWeight(uint32_t n);
+int hammingWeight(uint32_t n) {
+    int count = 0;
+    while (n) {
+        n &= (n - 1);
+        count++;
+    }
+    return count;
+}
