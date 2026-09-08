3f0daa38b96b9ffd3df5235b9bc669591c9bdb5c
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:23 +0530

    Create Question2.c

--- DSA/Aarav_590023538/Day_8/Question2.c
+++ DSA/Aarav_590023538/Day_8/Question2.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-// Reverse a String
 int main() {
-char s[1000];
+    char s[1000];
+
     // Read the string
     scanf("%s", s);
+
     // Find the length of the string
     int n = strlen(s);
+
     // Reverse the string by swapping characters
     for (int i = 0; i < n / 2; i++) {
+        char temp = s[i];
+        s[i] = s[n - 1 - i];
+        s[n - 1 - i] = temp;
+    }
+
+    // Print the reversed string
+    printf("%s\n", s);
+
     return 0;
 }
