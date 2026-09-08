24c080d8578ef95d76e7f8fd7a47ccae011399c8
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:39:37 +0530

    Implement solution logic for Day 85 Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_85/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_85/Question1_leetcode.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// LeetCode 455: Assign Cookies
-int findContentChildren(int* g, int gSize, int* s, int sSize);
+int compareInt(const void* a, const void* b) { return *(int*)a - *(int*)b; }
+int findContentChildren(int* g, int gSize, int* s, int sSize) {
+    qsort(g, gSize, sizeof(int), compareInt);
+    qsort(s, sSize, sizeof(int), compareInt);
+    int i = 0, j = 0;
+    while (i < gSize && j < sSize) {
+        if (s[j] >= g[i]) i++;
+        j++;
+    }
+    return i;
+}
