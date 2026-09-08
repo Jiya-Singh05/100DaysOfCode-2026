aca023b9eb05aad975588fa8588fe1c3b7ac8dfc
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:44 +0530

    Implement solution logic for Day 44 Question2.c

--- DSA/Aarav_590023538/Day_44/Question2.c
+++ DSA/Aarav_590023538/Day_44/Question2.c
@@ -2,5 +2,20 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 
-// Favourite Number
-string favouriteNumber(vector<int>& nums, int favoriteIndex, int k);
+pp
+#include <bits/stdc++.h>
+using namespace std;
+
+string favouriteNumber(vector<int>& nums, int favoriteIndex, int k) {
+    int val = nums[favoriteIndex - 1]; // convert to 0-indexed
+    int cntGreater = 0, cntEqual = 0;
+
+    for (int x : nums) {
+        if (x > val) cntGreater++;
+        else if (x == val) cntEqual++;
+    }
+
+    if (cntGreater + cntEqual <= k) return "YES";   // definitely removed
+    if (cntGreater >= k) return "NO";                // definitely not removed
+    return "MAYBE";                                  // depends on tie-breaking
+}
