2b11d1b80ad749eb95e7ddf0528e11f0e7dc37d2
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:42 +0530

    Implement solution logic for Day 16 Question2.c

--- DSA/Aarav_590023538/Day_16/Question2.c
+++ DSA/Aarav_590023538/Day_16/Question2.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Delete Last Node of a Doubly Linked List
-struct Node* deleteLastNode(struct Node *head);
-void printList(struct Node *head);
+// Structure of a doubly linked list node
+struct Node {
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+
+// Function to delete the last node
+struct Node* deleteLastNode(struct Node *head) {
+    // If the list is empty
+    if (head == NULL)
+        return NULL;
+    // If the list contains only one node
+    if (head->next == NULL) {
+        free(head);
+        return NULL;
+    }
+    // Traverse to the last node
+    struct Node *temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    // Remove the last node from the list
+    temp->prev->next = NULL;
+    // Free the memory of the deleted node
+    free(temp);
+    // Return the updated head
+    return head;
+}
+
+// Function to print the linked list
+void printList(struct Node *head) {
+    // If the list is empty
+    if (head == NULL) {
+        printf("NULL");
+        return;
+    }
+    // Print all nodes
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
