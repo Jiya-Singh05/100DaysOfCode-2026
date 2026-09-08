b8e5d49213d1054c0f24c7663e5ab2f57ca52be0
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:37:57 +0530

    Create Question2.c

--- DSA/Sahaj_590026086/Day_67/Question2.c
+++ DSA/Sahaj_590026086/Day_67/Question2.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-struct TreeNode { int val; struct TreeNode *left; struct TreeNode *right; };
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
 int checkHeight(struct TreeNode* root) {
     if (!root) return 0;
     int lh = checkHeight(root->left);
@@ -10,4 +16,14 @@ int checkHeight(struct TreeNode* root) {
     if (rh == -1) return -1;
     if (abs(lh - rh) > 1) return -1;
     return 1 + (lh > rh ? lh : rh);
+}
+
+bool isBalanced(struct TreeNode* root) {
+    return checkHeight(root) != -1;
+}
+
+int main() {
+    struct TreeNode r = {1, NULL, NULL};
+    printf("%s\n", isBalanced(&r) ? "true" : "false");
+    return 0;
 }
