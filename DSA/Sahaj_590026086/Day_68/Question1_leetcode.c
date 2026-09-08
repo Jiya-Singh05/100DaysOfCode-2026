4df10a67e286fc38b2a94a10bc3eac3737289d01
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:37:58 +0530

    Create Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_68/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_68/Question1_leetcode.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct TreeNode { int val; struct TreeNode *left; struct TreeNode *right; };
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
 void storeInorder(struct TreeNode* root, int* arr, int* sz) {
     if (!root) return;
     storeInorder(root->left, arr, sz);
     arr[(*sz)++] = root->val;
     storeInorder(root->right, arr, sz);
 }
+
 struct TreeNode* buildTree(int* arr, int start, int end) {
     if (start > end) return NULL;
     int mid = start + (end - start) / 2;
@@ -15,4 +22,17 @@ struct TreeNode* buildTree(int* arr, int start, int end) {
     node->left = buildTree(arr, start, mid - 1);
     node->right = buildTree(arr, mid + 1, end);
     return node;
+}
+
+struct TreeNode* balanceBST(struct TreeNode* root) {
+    int arr[1000]; int sz = 0;
+    storeInorder(root, arr, &sz);
+    return buildTree(arr, 0, sz - 1);
+}
+
+int main() {
+    struct TreeNode r = {1, NULL, NULL};
+    struct TreeNode* b = balanceBST(&r);
+    printf("%d\n", b->val);
+    return 0;
 }
