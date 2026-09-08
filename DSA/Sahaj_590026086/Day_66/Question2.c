9f6c4a0a4c8ea3c21ceeb5de0389caa35ecefda8
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:37:54 +0530

    Create Question2.c

--- DSA/Sahaj_590026086/Day_66/Question2.c
+++ DSA/Sahaj_590026086/Day_66/Question2.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct TreeNode { int val; struct TreeNode *left; struct TreeNode *right; };
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
 struct TreeNode* mergeTrees(struct TreeNode* root1, struct TreeNode* root2) {
     if (!root1) return root2;
     if (!root2) return root1;
@@ -8,4 +14,12 @@ struct TreeNode* mergeTrees(struct TreeNode* root1, struct TreeNode* root2) {
     root1->left = mergeTrees(root1->left, root2->left);
     root1->right = mergeTrees(root1->right, root2->right);
     return root1;
+}
+
+int main() {
+    struct TreeNode t1 = {1, NULL, NULL};
+    struct TreeNode t2 = {2, NULL, NULL};
+    struct TreeNode* res = mergeTrees(&t1, &t2);
+    printf("%d\n", res->val);
+    return 0;
 }
