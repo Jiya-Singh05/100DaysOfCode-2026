02a574820fc818a2d477cfd80fb3f0671496dd63
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:38:03 +0530

    Implement solution logic for Day 71 Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_71/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_71/Question1_leetcode.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// LeetCode 997: Find the Town Judge
-int findJudge(int n, int** trust, int trustSize, int* trustColSize);
+int findJudge(int n, int** trust, int trustSize, int* trustColSize) {
+    int* score = (int*)calloc(n + 1, sizeof(int));
+    for (int i = 0; i < trustSize; i++) {
+        score[trust[i][0]]--;
+        score[trust[i][1]]++;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (score[i] == n - 1) { free(score); return i; }
+    }
+    free(score);
+    return -1;
+}
