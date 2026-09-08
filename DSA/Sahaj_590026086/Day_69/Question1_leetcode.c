2bf9057f93fd1480af7d32b8f6d6932484fb146d
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:38:00 +0530

    Implement solution logic for Day 69 Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_69/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_69/Question1_leetcode.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// LeetCode 1008: Construct BST from Preorder Traversal
 struct TreeNode { int val; struct TreeNode *left; struct TreeNode *right; };
-struct TreeNode* bstFromPreorder(int* preorder, int preorderSize);
+struct TreeNode* build(int* preorder, int n, int* idx, int bound) {
+    if (*idx >= n || preorder[*idx] > bound) return NULL;
+    struct TreeNode* root = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    root->val = preorder[(*idx)++];
+    root->left = build(preorder, n, idx, root->val);
+    root->right = build(preorder, n, idx, bound);
+    return root;
+}
