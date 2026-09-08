b84bc949eac39f65c5ee9bfba428cc98c793c424
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:08 +0530

    Implement solution logic for Day 1 Question2.c

--- DSA/Aarav_590023538/Day_1/Question2.c
+++ DSA/Aarav_590023538/Day_1/Question2.c
@@ -3,6 +3,15 @@
 
 // Find the Sum of Array Elements
 int main() {
-    // Initial structure
+// Variable to store the size of the array
+    int n;
+    // Take the size of the array as input
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    // Declare the array
+    int arr[n];
+    // Variable to store the sum of elements
+    int sum=0;
+    // Take array elements as input from the user
     return 0;
 }
