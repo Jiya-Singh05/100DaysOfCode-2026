5529e10d50a01a3398b813363887710fce0d055e
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:39:18 +0530

    Implement solution logic for Day 78 Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_78/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_78/Question1_leetcode.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// LeetCode 136: Single Number
-int singleNumber(int* nums, int numsSize);
+int singleNumber(int* nums, int numsSize) {
+    int res = 0;
+    for (int i = 0; i < numsSize; i++) res ^= nums[i];
+    return res;
+}
