c496d2037cb4761cc9e52943244dbee190393da0
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:28 +0530

    Create initial structure for Day 66 Question1_Leetcode.c

---
+++ DSA/Aarav_590023538/Day_66/Question1_Leetcode.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+#include <stdlib.h>
+
+// Insert into a Binary Search Tree (LeetCode #701)
+TreeNode* buildTree(vector<int>& vals);
+TreeNode* insertIntoBST(TreeNode* root, int val);
