ab2415add9c5d23859349521efb6710fc8c5abd5
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:38:19 +0530

    Implement solution logic for Day 76 Question2.c

--- DSA/Sahaj_590026086/Day_76/Question2.c
+++ DSA/Sahaj_590026086/Day_76/Question2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
-// Does the k-th Line Graph Still Have an Euler Trail?
-bool hasEulerTrail(int* degrees, int n);
+bool hasEulerTrail(int* degrees, int n) {
+    int oddCount = 0;
+    for (int i = 0; i < n; i++) if (degrees[i] % 2 != 0) oddCount++;
+    return oddCount == 0 || oddCount == 2;
+}
