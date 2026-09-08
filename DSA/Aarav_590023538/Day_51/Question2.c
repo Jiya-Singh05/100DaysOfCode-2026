f7d71168abf0452028c6f2acfc426bec9cdbf82a
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:57 +0530

    Implement solution logic for Day 51 Question2.c

--- DSA/Aarav_590023538/Day_51/Question2.c
+++ DSA/Aarav_590023538/Day_51/Question2.c
@@ -2,5 +2,27 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 
-// Receiving Log Validation
-int firstInvalidRecord(vector<vector<string>>& deliveries);
+pp
+#include <bits/stdc++.h>
+using namespace std;
+
+int firstInvalidRecord(vector<vector<string>>& deliveries) {
+    // key: vendor_id + "#" + lot_number -> cert_code seen so far
+    unordered_map<string, string> seen;
+
+    for (int i = 0; i < (int)deliveries.size(); i++) {
+        const string& vendor = deliveries[i][0];
+        const string& lot    = deliveries[i][1];
+        const string& cert   = deliveries[i][2];
+
+        string key = vendor + "#" + lot;
+        auto it = seen.find(key);
+
+        if (it == seen.end()) {
+            seen[key] = cert;
+        } else if (it->second != cert) {
+            return i;
+        }
+    }
+    return -1;
+}
