580b0695433e62468161cb2baedbdecf7f7f6af3
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:38:08 +0530

    Implement solution logic for Day 73 Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_73/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_73/Question1_leetcode.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// LeetCode 2359: Find Closest Node to Given Two Nodes
-int closestMeetingNode(int* edges, int edgesSize, int node1, int node2);
+void bfsDist(int* edges, int n, int start, int* dist) {
+    for (int i = 0; i < n; i++) dist[i] = -1;
+    int d = 0, curr = start;
+    while (curr != -1 && dist[curr] == -1) {
+        dist[curr] = d++;
+        curr = edges[curr];
+    }
+}
