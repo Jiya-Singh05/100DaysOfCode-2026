53b3948d5fae74c2f9f8b33e80691b9be25caee7
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:22 +0530

    Implement solution logic for Day 8 Question2.c

--- DSA/Aarav_590023538/Day_8/Question2.c
+++ DSA/Aarav_590023538/Day_8/Question2.c
@@ -4,6 +4,12 @@
 
 // Reverse a String
 int main() {
-    // Initial structure
+char s[1000];
+    // Read the string
+    scanf("%s", s);
+    // Find the length of the string
+    int n = strlen(s);
+    // Reverse the string by swapping characters
+    for (int i = 0; i < n / 2; i++) {
     return 0;
 }
