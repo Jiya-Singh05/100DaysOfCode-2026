928778454bb3415b97e5945173bb156e5a734d74
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:46 +0530

    Implement solution logic for Day 45 Question2.c

--- DSA/Aarav_590023538/Day_45/Question2.c
+++ DSA/Aarav_590023538/Day_45/Question2.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Count Inversions in an Array
-public long countInv(int[] a);
-private long sort(int[] a, int[] tmp, int l, int r);
-private long merge(int[] a, int[] tmp, int l, int m, int r);
+java
+class Solution {
+    public long countInv(int[] a) {
+        int[] tmp = new int[a.length];
+        return sort(a, tmp, 0, a.length - 1);
+    }
+    
+    private long sort(int[] a, int[] tmp, int l, int r) {
+        long cnt = 0;
+        
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            
+            cnt += sort(a, tmp, l, m);
+            cnt += sort(a, tmp, m + 1, r);
+            cnt += merge(a, tmp, l, m, r);
+        }
+        
+        return cnt;
+    }
+    
+    private long merge(int[] a, int[] tmp, int l, int m, int r) {
+        int i = l, j = m + 1, k = l;
+        long cnt = 0;
+        
+        while (i <= m && j <= r) {
+            if (a[i] <= a[j]) {
+                tmp[k++] = a[i++];
+            } else {
+                tmp[k++] = a[j++];
+                cnt += (m - i + 1);
+            }
+        }
+        
+        while (i <= m) tmp[k++] = a[i++];
+        while (j <= r) tmp[k++] = a[j++];
+        
+        for (i = l; i <= r; i++) a[i] = tmp[i];
+        
+        return cnt;
+    }
+    
+    public static
