a1e39eee2ecc7cd02b75b4d59ea7d404b6b10dda
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:39:42 +0530

    Implement solution logic for Day 86 Question2.c

--- DSA/Sahaj_590026086/Day_86/Question2.c
+++ DSA/Sahaj_590026086/Day_86/Question2.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// Fractional Knapsack Problem
 typedef struct { long long value; long long weight; double ratio; } Item;
-double fractionalKnapsack(long long* value, long long* weight, int n, long long capacity);
+int compareItem(const void* a, const void* b) {
+    double r1 = ((Item*)a)->ratio;
+    double r2 = ((Item*)b)->ratio;
+    if (r2 > r1) return 1;
+    if (r2 < r1) return -1;
+    return 0;
+}
