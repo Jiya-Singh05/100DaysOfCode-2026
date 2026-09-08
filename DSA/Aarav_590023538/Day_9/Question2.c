9baa6c4e99635e8a419cbfc62fbcfe6821fb69b0
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:25 +0530

    Implement solution logic for Day 9 Question2.c

--- DSA/Aarav_590023538/Day_9/Question2.c
+++ DSA/Aarav_590023538/Day_9/Question2.c
@@ -3,6 +3,14 @@
 
 // Remove Spaces from String
 int main() {
-    // Initial structure
+char str[1000];
+    int i = 0, j = 0;
+    // Read the complete string including spaces
+    fgets(str, sizeof(str), stdin);
+    // Traverse the string
+    while (str[i] != '\0') {
+        // Copy only non-space characters
+        if (str[i] != ' ') {
+            str[j] = str[i];
     return 0;
 }
