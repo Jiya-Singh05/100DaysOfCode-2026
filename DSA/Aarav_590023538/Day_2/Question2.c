4e022d0380b9c1d2e0c30645c8d0c796dc9bcea4
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:10 +0530

    Implement solution logic for Day 2 Question2.c

--- DSA/Aarav_590023538/Day_2/Question2.c
+++ DSA/Aarav_590023538/Day_2/Question2.c
@@ -3,6 +3,16 @@
 
 // Find the Largest Element in an Array
 int main() {
-    // Initial structure
+// Variable to store the size of the array
+    int n;
+    // Take the size of the array as input
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    // Declare the array
+    int arr[n];
+    // Take array elements as input from the user
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     return 0;
 }
