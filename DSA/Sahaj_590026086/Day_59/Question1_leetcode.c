a409e4a62c49d882f9291b14562f371dd2434962
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:37:36 +0530

    Implement solution logic for Day 59 Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_59/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_59/Question1_leetcode.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
-// LeetCode 958: Check Completeness of a Binary Tree
 struct TreeNode { int val; struct TreeNode *left; struct TreeNode *right; };
-bool isCompleteTree(struct TreeNode* root);
+bool isCompleteTree(struct TreeNode* root) {
+    if (!root) return true;
+    struct TreeNode* queue[1000];
+    int front = 0, rear = 0;
+    queue[rear++] = root;
+    bool nullFound = false;
+    while (front < rear) {
+        struct TreeNode* curr = queue[front++];
+        if (!curr) nullFound = true;
+        else {
+            if (nullFound) return false;
+            queue[rear++] = curr->left;
+            queue[rear++] = curr->right;
+        }
+    }
+    return true;
+}
