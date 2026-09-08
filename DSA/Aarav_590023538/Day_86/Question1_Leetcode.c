fefcf2bda007e53fbcfc9ff60b3a7cd5607a17d1
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:29:07 +0530

    Implement solution logic for Day 86 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_86/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_86/Question1_Leetcode.c
@@ -3,5 +3,16 @@
 #include <string.h>
 #include <stdbool.h>
 
-// LeetCode 409: Longest Palindrome
-int longestPalindrome(char* s);
+int longestPalindrome(char* s) {
+    int freq[128] = {0};
+    int len = strlen(s);
+    for (int i = 0; i < len; i++) freq[(unsigned char)s[i]]++;
+    int result = 0;
+    bool hasOdd = false;
+    for (int i = 0; i < 128; i++) {
+        result += (freq[i] / 2) * 2;
+        if (freq[i] % 2 != 0) hasOdd = true;
+    }
+    if (hasOdd) result++;
+    return result;
+}
