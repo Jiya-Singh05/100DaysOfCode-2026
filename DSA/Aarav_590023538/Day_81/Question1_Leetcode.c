20f4e537493bd04eaa7a2f2578e6c052b99d08e6
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:59 +0530

    Implement solution logic for Day 81 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_81/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_81/Question1_Leetcode.c
@@ -2,5 +2,18 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 
-// Hamming Distance (LeetCode #461)
-int hammingDistance(int x, int y);
+pp
+#include <bits/stdc++.h>
+using namespace std;
+
+int hammingDistance(int x, int y) {
+    unsigned int diff = x ^ y;
+    int count = 0;
+
+    while (diff != 0) {
+        diff &= (diff - 1);
+        count++;
+    }
+
+    return count;
+}
