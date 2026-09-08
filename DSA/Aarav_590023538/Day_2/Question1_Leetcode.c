edce3491c21f4da3660bde410ba96a62e6559791
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:09 +0530

    Implement solution logic for Day 2 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_2/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_2/Question1_Leetcode.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Remove Element (LeetCode #27)
-int removeElement(int* nums, int numsSize, int val);
+// Function to remove all occurrences of val from the array in-place
+int removeElement(int* nums, int numsSize, int val) {
+    // Index to track position of next valid element
+    int k = 0;
+
+    // Traverse the array
+    for (int i = 0; i < numsSize; i++) {
+        // If current element is not val, keep it
+        if (nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    // Return count of remaining elements
+    return k;
+}
