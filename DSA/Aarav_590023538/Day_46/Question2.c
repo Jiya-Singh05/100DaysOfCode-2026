8d7b9ccea78234b5cf1d27917df53d22339ed8de
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:48 +0530

    Implement solution logic for Day 46 Question2.c

--- DSA/Aarav_590023538/Day_46/Question2.c
+++ DSA/Aarav_590023538/Day_46/Question2.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Maximum Prefix Difference Score
+python
+def max_prefix_diff_score(nums):
+    n = len(nums)
+    if n <= 1:
+        return 0
+
+    lo = hi = nums[0]
+    for x in nums[1:]:
+        if x < lo: lo = x
+        if x > hi: hi = x
+
+    return (n - 1) * (hi - lo)
