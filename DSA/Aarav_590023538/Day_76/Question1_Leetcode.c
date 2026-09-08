6cab449b9a3effe7b11ae10594bba9350c892c2d
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:49 +0530

    Create initial structure for Day 76 Question1_Leetcode.c

---
+++ DSA/Aarav_590023538/Day_76/Question1_Leetcode.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+
+// Count Ways to Build Rooms in an Ant Colony (LeetCode #1916)
+long long modPow(long long a, long long e);
+long long C(int n, int r);
+int waysToBuildRooms(vector<int>& prevRoom);
