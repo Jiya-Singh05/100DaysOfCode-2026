ecba4dea4cd650ee92b37f251d42d90bec756c26
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:50 +0530

    Create initial structure for Day 47 Question2.c

---
+++ DSA/Aarav_590023538/Day_47/Question2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <climits>
+
+// K-th Smallest Element in Two Sorted Arrays
+int findKthSmallest(vector<int>& A, vector<int>& B, int k);
