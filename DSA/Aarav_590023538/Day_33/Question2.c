f5da598a3ab965782058b3035947709c8556bd4e
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:22 +0530

    Implement solution logic for Day 33 Question2.c

--- DSA/Aarav_590023538/Day_33/Question2.c
+++ DSA/Aarav_590023538/Day_33/Question2.c
@@ -2,7 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#define MAX 100
 
-// Postfix to Prefix Conversion
-void push(char str[]);
-char* pop();
+// Stack to store strings
+char stack[MAX][MAX];
+int top = -1;
+
+// Push a string onto the stack
+void push(char str[]) {
+    strcpy(stack[++top], str);
+}
+
+// Pop a string from the stack
+char* pop() {
+    return stack[top--];
+}
