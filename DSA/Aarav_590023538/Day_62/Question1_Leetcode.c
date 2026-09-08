f4bdf8f71575bb343bc7e95e2e9b571a33744266
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:21 +0530

    Create initial structure for Day 62 Question1_Leetcode.c

---
+++ DSA/Aarav_590023538/Day_62/Question1_Leetcode.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+#include <stdlib.h>
+
+// Evaluate Boolean Binary Tree (LeetCode #2331)
+TreeNode* buildTree(vector<int>& vals);
+bool evaluateTree(TreeNode* root);
