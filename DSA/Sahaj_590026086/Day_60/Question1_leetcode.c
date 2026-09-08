5acc74f57faf8cf6e87782d43ab72da80cb09543
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:37:38 +0530

    Create initial structure for Day 60 Question1_leetcode.c

---
+++ DSA/Sahaj_590026086/Day_60/Question1_leetcode.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// LeetCode 543: Diameter of Binary Tree
+struct TreeNode { int val; struct TreeNode *left; struct TreeNode *right; };
+int diameterOfBinaryTree(struct TreeNode* root);
