ceb6ccc2b806852547199c243a76fe76e104c886
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:26 +0530

    Create Question2.c

--- DSA/Aarav_590023538/Day_34/Question2.c
+++ DSA/Aarav_590023538/Day_34/Question2.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define MAX 100
 
-// Maximum People Visible in a Line
 int main() {
-int arr[MAX], n;
+    int arr[MAX], n;
     int stack[MAX], top = -1;
     int left[MAX], right[MAX];
+
     printf("Enter number of people: ");
     scanf("%d", &n);
+
     printf("Enter heights:\n");
     for (int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
+
     // Find nearest greater or equal element on the left
     top = -1;
     for (int i = 0; i < n; i++) {
@@ -25,8 +27,35 @@ int arr[MAX], n;
         // Push current index
         stack[++top] = i;
     }
+
     // Find nearest greater or equal element on the right
     top = -1;
     for (int i = n - 1; i >= 0; i--) {
+        // Remove all smaller heights
+        while (top != -1 && arr[stack[top]] < arr[i])
+            top--;
+        // Store index of nearest greater/equal element
+        if (top == -1)
+            right[i] = n;
+        else
+            right[i] = stack[top];
+        // Push current index
+        stack[++top] = i;
+    }
+
+    int maxVisible = 0;
+    // Calculate visible people for every person
+    for (int i = 0; i < n; i++) {
+        // People visible on the left
+        int leftCount = i - left[i] - 1;
+        // People visible on the right
+        int rightCount = right[i] - i - 1;
+        // Include the person themselves
+        int total = leftCount + rightCount + 1;
+        if (total > maxVisible)
+            maxVisible = total;
+    }
+
+    printf("Maximum people visible = %d\n", maxVisible);
     return 0;
 }
