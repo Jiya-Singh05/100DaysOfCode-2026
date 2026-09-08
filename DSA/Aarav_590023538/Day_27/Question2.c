f505027968873a6791bdc4a20a5b8bfcb922900f
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:10 +0530

    Implement solution logic for Day 27 Question2.c

--- DSA/Aarav_590023538/Day_27/Question2.c
+++ DSA/Aarav_590023538/Day_27/Question2.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-// Subarrays with Bounded Maximum
-int countSubarrays(int arr[], int n, int limit);
+// Function to count subarrays whose maximum element
+// is less than or equal to the given limit
+int countSubarrays(int arr[], int n, int limit) {
+    int count = 0;
+    int length = 0;
+
+    for (int i = 0; i < n; i++) {
+        // If the current element is within the limit,
+        // increase the current valid segment length
+        if (arr[i] <= limit) {
+            length++;
+        } else {
+            // Otherwise, start a new segment
+            length = 0;
+        }
+        // Add the number of valid subarrays
+        // ending at the current index
+        count += length;
+    }
+
+    return count;
+}
