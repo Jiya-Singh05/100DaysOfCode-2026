71706c7c2bbd0cc6e2cd9ab78ff3c4f77d100031
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:37:52 +0530

    Implement solution logic for Day 66 Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_66/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_66/Question1_leetcode.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// LeetCode 701: Insert into a Binary Search Tree
 struct TreeNode { int val; struct TreeNode *left; struct TreeNode *right; };
-struct TreeNode* insertIntoBST(struct TreeNode* root, int val);
+struct TreeNode* createNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val; node->left = node->right = NULL;
+    return node;
+}
