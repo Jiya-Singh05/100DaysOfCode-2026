54fccf0e00f9fab34b35a6582e3746c41371f2ce
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:18 +0530

    Implement solution logic for Day 32 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_32/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_32/Question1_Leetcode.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Minimum String Length After Removing Substrings (LeetCode #2696)
-int minLength(char* s);
+int minLength(char* s) {
+    // Stack to store characters
+    char stack[101];
+    int top = -1;
+    // Traverse the given string
+    for (int i = 0; s[i] != '\0'; i++) {
+        // Check if the top of the stack and current character
+        // form "AB" or "CD"
+        if (top >= 0 &&
+            ((stack[top] == 'A' && s[i] == 'B') ||
+             (stack[top] == 'C' && s[i] == 'D'))) {
+            // Remove the matching pair
+            top--;
+        } else {
+            // Otherwise, push the current character onto the stack
+            stack[++top] = s[i];
+        }
+    }
+    // Remaining characters in the stack
+    // represent the minimum possible string length
+    return top + 1;
+}
