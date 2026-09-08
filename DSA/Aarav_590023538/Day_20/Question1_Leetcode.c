d6fd818500b50fccd40937132d5f286b1c304874
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:51 +0530

    Implement solution logic for Day 20 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_20/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_20/Question1_Leetcode.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Remove Linked List Elements (LeetCode #203)
-struct ListNode* removeElements(struct ListNode* head, int val);
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    // Create an extra node to handle deletion of the head node easily
+    struct ListNode start;
+    start.next = head;
+    // Pointer to traverse the list
+    struct ListNode *current = &start;
+    // Traverse the linked list
+    while (current->next != NULL) {
+        // If the next node contains the value to be removed
+        if (current->next->val == val) {
+            struct ListNode *temp = current->next;  // Node to delete
+            current->next = temp->next;             // Skip the node
+            free(temp);                             // Free memory
+        } else {
+            // Move to the next node
+            current = current->next;
+        }
+    }
+    // Return the updated head
+    return start.next;
+}
