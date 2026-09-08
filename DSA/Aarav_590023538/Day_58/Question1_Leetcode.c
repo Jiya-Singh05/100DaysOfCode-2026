25fa7ce6866c44d433be8151828d72571dfd53c4
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:11 +0530

    Implement solution logic for Day 58 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_58/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_58/Question1_Leetcode.c
@@ -1,4 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Check Completeness of a Binary Tree
+python
+from collections import deque
+
+class Solution:
+    def isCompleteTree(self, root: 'TreeNode') -> bool:
+        if not root:
+            return True
+
+        queue = deque([root])
+        seen_null = False
+
+        while queue:
+            node = queue.popleft()
+            if node is None:
+                seen_null = True
+                continue
+            if seen_null:
+                return False
+            queue.append(node.left)
+            queue.append(node.right)
+
+        return True
