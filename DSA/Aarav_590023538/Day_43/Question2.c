29a865c5d9ad1098fdf02d0a18aa797189c625ee
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:41 +0530

    Implement solution logic for Day 43 Question2.c

--- DSA/Aarav_590023538/Day_43/Question2.c
+++ DSA/Aarav_590023538/Day_43/Question2.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Maximize Sum of Pair Minimums
+python
+def maxSumPairMin(nums):
+    nums.sort()
+    return sum(nums[0::2])  # sum every element at even index (0, 2, 4, ...)
+
+
+# Test cases
+print(maxSumPairMin([1, 4, 3, 2]))       # 4
+print(maxSumPairMin([6, 2, 6, 5, 1, 2])) # 9
+print(maxSumPairMin([1, 2, 3, 4]))       # 4
