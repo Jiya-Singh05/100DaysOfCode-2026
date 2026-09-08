6c2f1a69c13f35bd62a8af102e78324bb554fc4a
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:37:40 +0530

    Implement solution logic for Day 60 Question2.c

--- DSA/Sahaj_590026086/Day_60/Question2.c
+++ DSA/Sahaj_590026086/Day_60/Question2.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// The Mountain Expedition
-int maxSubarraySum(int* arr, int n);
+int maxSubarraySum(int* arr, int n) {
+    int max_so_far = arr[0], curr_max = arr[0];
+    for (int i = 1; i < n; i++) {
+        curr_max = (arr[i] > curr_max + arr[i]) ? arr[i] : curr_max + arr[i];
+        if (curr_max > max_so_far) max_so_far = curr_max;
+    }
+    return max_so_far;
+}
