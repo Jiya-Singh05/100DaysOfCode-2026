8426e02cbe0e7a41dd575e257c5466c39b5e87a3
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:21 +0530

    Implement solution logic for Day 8 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_8/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_8/Question1_Leetcode.c
@@ -1,7 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
-// Valid Palindrome (LeetCode #125)
-bool isPalindrome(char *s);
+bool isPalindrome(char *s) {
+    char str[100000];
+    int k = 0;
+
+    // Copy only letters and digits
+    for (int i = 0; s[i] != '\0'; i++) {
+        // Convert uppercase to lowercase
+        if (s[i] >= 'A' && s[i] <= 'Z') {
+            str[k++] = s[i] + 32;
+        }
+        // Store lowercase letters
+        else if (s[i] >= 'a' && s[i] <= 'z') {
+            str[k++] = s[i];
+        }
+        // Store digits
+        else if (s[i] >= '0' && s[i] <= '9') {
+            str[k++] = s[i];
+        }
+    }
+
+    // Check palindrome
+    int left = 0;
+    int right = k - 1;
+    while (left < right) {
+        if (str[left] != str[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
