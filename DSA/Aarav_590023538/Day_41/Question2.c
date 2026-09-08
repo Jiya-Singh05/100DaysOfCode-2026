185de8866f50eb65455dbed65fcf577599addc09
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:38 +0530

    Implement solution logic for Day 41 Question2.c

--- DSA/Aarav_590023538/Day_41/Question2.c
+++ DSA/Aarav_590023538/Day_41/Question2.c
@@ -2,5 +2,43 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 
-// Office Printer Wars
-string officePrinterWars(string jobs);
+pp
+#include <bits/stdc++.h>
+using namespace std;
+
+string officePrinterWars(string jobs) {
+    queue<char> q;
+    for (char c : jobs) q.push(c);
+
+    int banS = 0; // pending cancellations Sales owes to Marketing
+    int banM = 0; // pending cancellations Marketing owes to Sales
+    int countS = count(jobs.begin(), jobs.end(), 'S');
+    int countM = (int)jobs.size() - countS;
+
+    while (countS > 0 && countM > 0) {
+        char job = q.front();
+        q.pop();
+
+        if (job == 'S') {
+            if (banS > 0) {
+                // this Sales job gets cancelled by a pending Marketing ban
+                banS--;
+                countS--;
+            } else {
+                // process it, Sales now owes Marketing a cancellation
+                banM++;
+                q.push('S');
+            }
+        } else { // job == 'M'
+            if (banM > 0) {
+                banM--;
+                countM--;
+            } else {
+                banS++;
+                q.push('M');
+            }
+        }
+    }
+
+    return countS > 0 ? "Sales" : "Marketing";
+}
