aba7dc9b899a2ad8d7c7fa34b13e42f4309dfc6c
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:37:55 +0530

    Implement solution logic for Day 67 Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_67/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_67/Question1_leetcode.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// LeetCode 450: Delete Node in a BST
 struct TreeNode { int val; struct TreeNode *left; struct TreeNode *right; };
-struct TreeNode* deleteNode(struct TreeNode* root, int key);
+struct TreeNode* getMin(struct TreeNode* node) {
+    while (node->left) node = node->left;
+    return node;
+}
