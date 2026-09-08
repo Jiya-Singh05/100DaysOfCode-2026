7b79a46151a4f98947b4ee25c1398cdd60edeb29
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:37:29 +0530

    Implement solution logic for Day 55 Question2.c

--- DSA/Sahaj_590026086/Day_55/Question2.c
+++ DSA/Sahaj_590026086/Day_55/Question2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Maximum Playlist Pleasure
-long long maxPleasure(int* length, int* beauty, int n, int k);
+typedef struct { int len; int bty; } Song;
+int compareSong(const void* a, const void* b) { return ((Song*)b)->bty - ((Song*)a)->bty; }
