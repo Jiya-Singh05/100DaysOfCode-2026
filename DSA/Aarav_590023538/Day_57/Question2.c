40559e3e5f336c969dd56ed637423664af443877
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:09 +0530

    Create initial structure for Day 57 Question2.c

---
+++ DSA/Aarav_590023538/Day_57/Question2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+#include <stdlib.h>
+
+// The Twin Warehouses
+bool isMirror(const vector<long long>& a, const vector<long long>& b,
+              int i, int j);
