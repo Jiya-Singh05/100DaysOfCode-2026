7ef3ff8929eec362499eb45c52306b9836de1763
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:50 +0530

    Implement solution logic for Day 76 Question2.c

--- DSA/Aarav_590023538/Day_76/Question2.c
+++ DSA/Aarav_590023538/Day_76/Question2.c
@@ -2,5 +2,21 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 
-// Does the k-th Line Graph Still Have an Euler Trail?
-bool hasEulerTrail(int n, const vector<pair<int, int>>& edges);
+pp
+#include <bits/stdc++.h>
+using namespace std;
+
+bool hasEulerTrail(int n, const vector<pair<int, int>>& edges) {
+    vector<int> degree(n + 1, 0);
+
+    for (auto [u, v] : edges) {
+        degree[u]++;
+        degree[v]++;
+    }
+
+    int odd = 0;
+    for (int i = 1; i <= n; i++)
+        odd += (degree[i] & 1);
+
+    return odd == 0 || odd == 2;
+}
