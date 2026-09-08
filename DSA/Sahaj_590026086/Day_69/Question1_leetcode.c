c9b0e5685ef08fc311369747ec1a12fced139fc3
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:38:01 +0530

    Create Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_69/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_69/Question1_leetcode.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct TreeNode { int val; struct TreeNode *left; struct TreeNode *right; };
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
 struct TreeNode* build(int* preorder, int n, int* idx, int bound) {
     if (*idx >= n || preorder[*idx] > bound) return NULL;
     struct TreeNode* root = (struct TreeNode*)malloc(sizeof(struct TreeNode));
@@ -8,4 +14,16 @@ struct TreeNode* build(int* preorder, int n, int* idx, int bound) {
     root->left = build(preorder, n, idx, root->val);
     root->right = build(preorder, n, idx, bound);
     return root;
+}
+
+struct TreeNode* bstFromPreorder(int* preorder, int preorderSize) {
+    int idx = 0;
+    return build(preorder, preorderSize, &idx, 1e9);
+}
+
+int main() {
+    int p[] = {8, 5, 1, 7, 10, 12};
+    struct TreeNode* r = bstFromPreorder(p, 6);
+    printf("%d\n", r->val);
+    return 0;
 }
