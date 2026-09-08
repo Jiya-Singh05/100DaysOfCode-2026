87b915f3b1b9857c02186c690f193e51f77a5d16
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:51 +0530

    Create Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_48/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_48/Question1_Leetcode.c
@@ -25,3 +25,22 @@ static void printMatrix(int **mat, int m, int n) {
     }
     printf("]\n");
 }
+
+int main(void) {
+    // Sample 1
+    int r0[] = {10, 6, 9, 1};
+    int r1[] = {7, 5, 11, 2};
+    int r2[] = {4, 8, 3, 15};
+    int *mat1[] = {r0, r1, r2};
+    sortTheStudents(mat1, 3, 4, 2);
+    printMatrix(mat1, 3, 4); // [[7,5,11,2],[10,6,9,1],[4,8,3,15]]
+
+    // Sample 2
+    int s0[] = {3, 4};
+    int s1[] = {5, 6};
+    int *mat2[] = {s0, s1};
+    sortTheStudents(mat2, 2, 2, 0);
+    printMatrix(mat2, 2, 2); // [[5,6],[3,4]]
+
+    return 0;
+}
