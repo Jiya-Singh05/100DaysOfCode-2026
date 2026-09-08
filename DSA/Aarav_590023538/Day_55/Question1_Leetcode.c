5844197813df732e9fbd1fd6cc5fef63cf85d132
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:06 +0530

    Implement solution logic for Day 55 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_55/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_55/Question1_Leetcode.c
@@ -2,5 +2,32 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 
-// Find Subsequence of Length K With the Largest Sum (LeetCode #2099)
-void printVec(const vector<int>& v);
+pp
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> maxSubsequence(vector<int>& nums, int k) {
+    int n = nums.size();
+    vector<int> idx(n);
+    iota(idx.begin(), idx.end(), 0);
+
+    // Stable sort indices by value descending (ties keep original order)
+    stable_sort(idx.begin(), idx.end(), [&](int a, int b) {
+        return nums[a] > nums[b];
+    });
+
+    vector<int> chosen(idx.begin(), idx.begin() + k);
+    sort(chosen.begin(), chosen.end()); // restore original relative order
+
+    vector<int> result;
+    for (int i : chosen) result.push_back(nums[i]);
+    return result;
+}
+
+void printVec(const vector<int>& v) {
+    cout << "[";
+    for (size_t i = 0; i < v.size(); i++) {
+        cout << v[i] << (i + 1 == v.size() ? "" : ",");
+    }
+    cout << "]\n";
+}
