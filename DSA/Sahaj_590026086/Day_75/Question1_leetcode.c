e99784cf2671e3014d11f79412d019f4f4af632d
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:38:14 +0530

    Create initial structure for Day 75 Question1_leetcode.c

---
+++ DSA/Sahaj_590026086/Day_75/Question1_leetcode.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// LeetCode 1857: Largest Color Value in a Directed Graph
+int largestPathValue(char * colors, int** edges, int edgesSize, int* edgesColSize);
