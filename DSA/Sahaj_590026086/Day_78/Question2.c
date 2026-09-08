145db36836cf4683f96a3a97f34329486b15f9a7
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:39:20 +0530

    Implement solution logic for Day 78 Question2.c

--- DSA/Sahaj_590026086/Day_78/Question2.c
+++ DSA/Sahaj_590026086/Day_78/Question2.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// Maximum XOR Pair
-int maxXorPair(int* nums, int numsSize);
+int maxXorPair(int* nums, int numsSize) {
+    int maxVal = 0;
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            int val = nums[i] ^ nums[j];
+            if (val > maxVal) maxVal = val;
+        }
+    }
+    return maxVal;
+}
