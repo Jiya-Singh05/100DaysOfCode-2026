c7f01325b4d7ebfd3c8757b8a8091b9f7535a67d
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:29:08 +0530

    Create initial structure for Day 86 Question2.c

---
+++ DSA/Aarav_590023538/Day_86/Question2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Fractional Knapsack
+int compareItem(const void* a, const void* b);
+double fractionalKnapsack(long long* value, long long* weight, int n, long long capacity);
