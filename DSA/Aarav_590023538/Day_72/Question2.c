88e484313212bd0f04b0c2e4da959a9a7c43816c
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:40 +0530

    Create initial structure for Day 72 Question2.c

---
+++ DSA/Aarav_590023538/Day_72/Question2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Detective Aditya's Chase
+int cmp(const void *a, const void *b);
+int isMainRoad(int u, int v);
+void unlinkNode(int v);
+void solve(void);
