50f88912935df376632e9d5ccef60a7dbdaaf221
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:55 +0530

    Create Question2.c

--- DSA/Aarav_590023538/Day_79/Question2.c
+++ DSA/Aarav_590023538/Day_79/Question2.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <bits/stdc++.h>
-#include <stdlib.h>
-
 pp
 #include <bits/stdc++.h>
 using namespace std;
@@ -13,4 +9,17 @@ int findSingleNumber(const vector<int>& nums) {
         ans ^= x;
 
     return ans;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+
+    cout << findSingleNumber(nums) << "\n";
+    return 0;
 }
