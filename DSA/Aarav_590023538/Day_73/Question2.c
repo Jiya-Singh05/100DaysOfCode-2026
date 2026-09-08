7b4ed8da82c7aebe1d492a76fae75333b1b2ce98
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:43 +0530

    Implement solution logic for Day 73 Question2.c

--- DSA/Aarav_590023538/Day_73/Question2.c
+++ DSA/Aarav_590023538/Day_73/Question2.c
@@ -2,4 +2,32 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 
-// Detective Aditya's Chase
+pp
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> complementBFS(int n, int s, vector<unordered_set<int>>& adj) {
+    vector<int> dist(n + 1, -1);
+    dist[s] = 0;
+
+    set<int> unvisited;
+    for (int i = 1; i <= n; i++) if (i != s) unvisited.insert(i);
+
+    queue<int> q;
+    q.push(s);
+
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+
+        vector<int> toRemove;
+        for (int v : unvisited) {
+            if (adj[u].find(v) == adj[u].end()) {
+                dist[v] = dist[u] + 1;
+                toRemove.push_back(v);
+                q.push(v);
+            }
+        }
+        for (int v : toRemove) unvisited.erase(v);
+    }
+    return dist;
+}
