1ed139ebd03e3a41ad45d6b1b0b1176eefb65afe
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:51 +0530

    Implement solution logic for Day 78 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_78/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_78/Question1_Leetcode.c
@@ -2,5 +2,15 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 
-// Single Number (LeetCode #136)
-int singleNumber(vector<int>& nums);
+pp
+#include <bits/stdc++.h>
+using namespace std;
+
+int singleNumber(vector<int>& nums) {
+    int ans = 0;
+
+    for (int x : nums)
+        ans ^= x;
+
+    return ans;
+}
