ed7a0789d006651164655b6b9087373edd8d0781
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:39 +0530

    Implement solution logic for Day 72 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_72/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_72/Question1_Leetcode.c
@@ -2,5 +2,42 @@
 #include <stdlib.h>
 #include <limits.h>
 
-// Find Closest Node to Given Two Nodes (LeetCode #2359)
-int closestMeetingNode(int *edges, int n, int node1, int node2);
+int closestMeetingNode(int *edges, int n, int node1, int node2) {
+    int *dist1 = malloc(sizeof(int) * n);
+    int *dist2 = malloc(sizeof(int) * n);
+
+    for (int i = 0; i < n; i++) {
+        dist1[i] = -1;
+        dist2[i] = -1;
+    }
+
+    // Walk from node1
+    int cur = node1, d = 0;
+    while (cur != -1 && dist1[cur] == -1) {
+        dist1[cur] = d++;
+        cur = edges[cur];
+    }
+
+    // Walk from node2
+    cur = node2, d = 0;
+    while (cur != -1 && dist2[cur] == -1) {
+        dist2[cur] = d++;
+        cur = edges[cur];
+    }
+
+    int best = -1;
+    long long bestMax = LLONG_MAX;
+
+    for (int i = 0; i < n; i++) {
+        if (dist1[i] == -1 || dist2[i] == -1) continue;
+        long long m = dist1[i] > dist2[i] ? dist1[i] : dist2[i];
+        if (m < bestMax) {
+            bestMax = m;
+            best = i;
+        }
+    }
+
+    free(dist1);
+    free(dist2);
+    return best;
+}
