9d9338b9f94c282858824b01f766313726077b23
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:54 +0530

    Implement solution logic for Day 22 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_22/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_22/Question1_Leetcode.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Move Zeroes (LeetCode #283)
-void moveZeroes(int* nums, int numsSize);
+void moveZeroes(int* nums, int numsSize)
+{
+    int j = 0; // Position to place the next non-zero element
+    // Traverse the array
+    for (int i = 0; i < numsSize; i++)
+    {
+        // If the current element is non-zero
+        if (nums[i] != 0)
+        {
+            // Swap the current element with the element at index j
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            // Move j to the next position
+            j++;
+        }
+    }
+}
