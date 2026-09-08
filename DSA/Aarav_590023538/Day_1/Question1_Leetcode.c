d4716968c2427d6dba3145645fc5b086b6778129
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:06 +0530

    Implement solution logic for Day 1 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_1/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_1/Question1_Leetcode.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Missing Number
-int missingNumber(int nums[], int numsSize);
+// Function to find the missing number
+int missingNumber(int nums[], int numsSize) {
+    // Sum of numbers from 0 to n
+    int Total = numsSize * (numsSize + 1) / 2;
+    
+    // Calculate actual sum of array elements
+    int actualSum = 0;
+    for (int i = 0; i < numsSize; i++) {
+        actualSum += nums[i];
+    }
+
+    // Missing number = Total - Actual Sum
+    return Total - actualSum;
+}
