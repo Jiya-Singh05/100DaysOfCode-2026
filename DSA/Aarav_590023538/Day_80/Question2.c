1a0540358e840441aef1856336508718b169b6b7
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:58 +0530

    Create Question2.c

--- DSA/Aarav_590023538/Day_80/Question2.c
+++ DSA/Aarav_590023538/Day_80/Question2.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
-#include <bits/stdc++.h>
-#include <stdlib.h>
-
 pp
 #include <bits/stdc++.h>
 using namespace std;
 
 bool isPowerOfTwoOrZero(int n) {
     return n == 0 || (n & (n - 1)) == 0;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << (isPowerOfTwoOrZero(n) ? "true" : "false") << "\n";
+    return 0;
 }
