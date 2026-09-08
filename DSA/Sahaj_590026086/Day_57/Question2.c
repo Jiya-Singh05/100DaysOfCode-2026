a5710ba5a6a465325ac0cf8fb7ce125348bf17f4
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:37:32 +0530

    Implement solution logic for Day 57 Question2.c

--- DSA/Sahaj_590026086/Day_57/Question2.c
+++ DSA/Sahaj_590026086/Day_57/Question2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// The Twin Warehouses
 struct Node { int val; struct Node *left; struct Node *right; };
-int sumTwinTrees(struct Node* t1, struct Node* t2);
+int sumTwinTrees(struct Node* t1, struct Node* t2) {
+    if (!t1 || !t2) return 0;
+    return (t1->val == t2->val ? t1->val : 0) + sumTwinTrees(t1->left, t2->left) + sumTwinTrees(t1->right, t2->right);
+}
