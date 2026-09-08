ccbaf296bc462a2ac465dc2737e38278f5353aca
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:29:06 +0530

    Implement solution logic for Day 85 Question2.c

--- DSA/Aarav_590023538/Day_85/Question2.c
+++ DSA/Aarav_590023538/Day_85/Question2.c
@@ -2,5 +2,28 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 
-// Maximum Number of Activities
-int maxActivities(vector<int>& start, vector<int>& end);
+pp
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxActivities(vector<int>& start, vector<int>& end) {
+    int n = start.size();
+    vector<pair<int, int>> activities;
+
+    for (int i = 0; i < n; i++)
+        activities.push_back({end[i], start[i]});
+
+    sort(activities.begin(), activities.end());
+
+    int count = 1;
+    int lastEnd = activities[0].first;
+
+    for (int i = 1; i < n; i++) {
+        if (activities[i].second >= lastEnd) {
+            count++;
+            lastEnd = activities[i].first;
+        }
+    }
+
+    return count;
+}
