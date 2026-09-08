931bc198debc6ae9b1f67fb9cd427d3bc6221671
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:10 +0530

    Create Question2.c

--- DSA/Aarav_590023538/Day_57/Question2.c
+++ DSA/Aarav_590023538/Day_57/Question2.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <bits/stdc++.h>
-#include <stdlib.h>
-
 pp
 #include <bits/stdc++.h>
 using namespace std;
@@ -29,4 +25,32 @@ bool isMirror(const vector<long long>& a, const vector<long long>& b,
     // right of A <-> left of B
     return isMirror(a, b, 2 * i + 1, 2 * j + 2) &&
            isMirror(a, b, 2 * i + 2, 2 * j + 1);
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n1;
+        cin >> n1;
+
+        vector<long long> warehouse1(n1);
+        for (int i = 0; i < n1; i++)
+            cin >> warehouse1[i];
+
+        int n2;
+        cin >> n2;
+
+        vector<long long> warehouse2(n2);
+        for (int i = 0; i < n2; i++)
+            cin >> warehouse2[i];
+
+        if (isMirror(warehouse1, warehouse2, 0, 0))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
 }
