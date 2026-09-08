bb4ddc29fe4b09c1be5503285f1bf9d55052b7f0
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:47 +0530

    Implement solution logic for Day 75 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_75/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_75/Question1_Leetcode.c
@@ -2,5 +2,40 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 
-// Largest Color Value in a Directed Graph (LeetCode #1857)
-int largestPathValue(string colors, vector<vector<int>>& edges);
+pp
+#include <bits/stdc++.h>
+using namespace std;
+
+int largestPathValue(string colors, vector<vector<int>>& edges) {
+    int n = colors.size();
+    vector<vector<int>> adj(n);
+    vector<int> indegree(n, 0);
+    for (auto& e : edges) {
+        adj[e[0]].push_back(e[1]);
+        indegree[e[1]]++;
+    }
+
+    vector<array<int, 26>> count(n);
+    for (auto& row : count) row.fill(0);
+
+    queue<int> q;
+    for (int i = 0; i < n; i++) if (indegree[i] == 0) q.push(i);
+
+    int visited = 0, ans = 0;
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        visited++;
+
+        int c = colors[u] - 'a';
+        count[u][c]++;
+        ans = max(ans, count[u][c]);
+
+        for (int v : adj[u]) {
+            for (int k = 0; k < 26; k++)
+                count[v][k] = max(count[v][k], count[u][k]);
+            if (--indegree[v] == 0) q.push(v);
+        }
+    }
+
+    return visited == n ? ans : -1;
+}
