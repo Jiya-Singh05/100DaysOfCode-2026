f55aee3505b6df111629e01e398ea1cd0cd7fe3e
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:39:35 +0530

    Create Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_83/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_83/Question1_leetcode.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int singleNumber(int* nums, int numsSize) {
     int ones = 0, twos = 0;
     for (int i = 0; i < numsSize; i++) {
@@ -7,4 +8,10 @@ int singleNumber(int* nums, int numsSize) {
         twos = (twos ^ nums[i]) & ~ones;
     }
     return ones;
+}
+
+int main() {
+    int nums[] = {2, 2, 3, 2};
+    printf("%d\n", singleNumber(nums, 4));
+    return 0;
 }
