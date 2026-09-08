45dc583708efb0efa5834858d67f4a30974e52b4
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:01 +0530

    Implement solution logic for Day 53 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_53/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_53/Question1_Leetcode.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Substrings of Size Three with Distinct Characters
-public int countGoodSubstrings(String s);
+java
+class Solution {
+    public int countGoodSubstrings(String s) {
+        int count = 0;
+        for (int i = 0; i + 2 < s.length(); i++) {
+            char a = s.charAt(i), b = s.charAt(i + 1), c = s.charAt(i + 2);
+            if (a != b && b != c && a != c) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
