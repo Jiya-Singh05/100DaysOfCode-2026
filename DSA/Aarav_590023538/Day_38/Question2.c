feb3acc379fc7a941bc2651c01488f0ea409b3f5
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:32 +0530

    Implement solution logic for Day 38 Question2.c

--- DSA/Aarav_590023538/Day_38/Question2.c
+++ DSA/Aarav_590023538/Day_38/Question2.c
@@ -1,4 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// The Card Trick
+java
+import java.util.*;
+
+class Solution {
+    public int[] deckArrangement(int n) {
+        int[] result = new int[n];
+        Deque<Integer> positions = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            positions.addLast(i);
+        }
+        
+        int value = 1;
+        while (!positions.isEmpty()) {
+            // Reveal the position at the front
+            int pos = positions.pollFirst();
+            result[pos] = value++;
+            
+            // Move next position to the bottom
+            if (!positions.isEmpty()) {
+                positions.addLast(positions.pollFirst());
+            }
+        }
+        
+        return result;
+    }
+}
