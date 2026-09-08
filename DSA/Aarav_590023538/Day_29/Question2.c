4c99ae0b56585ea63068140fd5350c30d03b1715
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:12 +0530

    Implement solution logic for Day 29 Question2.c

--- DSA/Aarav_590023538/Day_29/Question2.c
+++ DSA/Aarav_590023538/Day_29/Question2.c
@@ -4,6 +4,14 @@
 
 // Reverse a String Using Stack
 int main() {
-    // Initial structure
+char str[100];
+    // Take input string
+    printf("Enter a string: ");
+    scanf("%s", str);
+    int n = strlen(str);
+    // Stack to store characters
+    char stack[100];
+    int top = -1;
+    // Push all characters of the string onto the stack
     return 0;
 }
