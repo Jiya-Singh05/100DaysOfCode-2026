1f3a393063d81e1da6aecf211dbafdb5052091a2
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:38:11 +0530

    Implement solution logic for Day 74 Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_74/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_74/Question1_leetcode.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// LeetCode 2316: Count Unreachable Pairs of Nodes in an Undirected Graph
-long long countPairs(int n, int** edges, int edgesSize, int* edgesColSize);
+int findParent(int* parent, int i) {
+    if (parent[i] == i) return i;
+    return parent[i] = findParent(parent, parent[i]);
+}
+void unionNodes(int* parent, int* rank, int i, int j) {
+    int rootI = findParent(parent, i);
+    int rootJ = findParent(parent, j);
+    if (rootI != rootJ) { parent[rootI] = rootJ; }
+}
