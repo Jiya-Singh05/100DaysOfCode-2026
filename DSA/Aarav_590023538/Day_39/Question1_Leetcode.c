f09d8b713201e48c7d07fe91757dc488be37129c
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:33 +0530

    Create Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_39/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_39/Question1_Leetcode.c
@@ -13,3 +13,8 @@ def time_required_to_buy(tickets, k):
             time += 1
         i = (i + 1) % n
     return time
+
+int main() {
+    printf("Execution completed successfully.\n");
+    return 0;
+}
