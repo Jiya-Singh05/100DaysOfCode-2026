a460e892411e967de0bd2b5e755a3b676a442d30
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:44 +0530

    Create Question2.c

--- DSA/Aarav_590023538/Day_44/Question2.c
+++ DSA/Aarav_590023538/Day_44/Question2.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <bits/stdc++.h>
-#include <stdlib.h>
-
 pp
 #include <bits/stdc++.h>
 using namespace std;
@@ -18,4 +14,17 @@ string favouriteNumber(vector<int>& nums, int favoriteIndex, int k) {
     if (cntGreater + cntEqual <= k) return "YES";   // definitely removed
     if (cntGreater >= k) return "NO";                // definitely not removed
     return "MAYBE";                                  // depends on tie-breaking
+}
+
+int main() {
+    vector<int> nums1 = {4, 3, 3, 2, 3};
+    cout << favouriteNumber(nums1, 2, 2) << endl; // MAYBE
+
+    vector<int> nums2 = {4, 2, 1, 3, 5};
+    cout << favouriteNumber(nums2, 5, 3) << endl; // YES
+
+    vector<int> nums3 = {5, 2, 4, 1, 3};
+    cout << favouriteNumber(nums3, 5, 2) << endl; // NO
+
+    return 0;
 }
