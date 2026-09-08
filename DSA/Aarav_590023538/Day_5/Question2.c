b251450fe41beec450519e735af35ab800c4cde8
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:17 +0530

    Implement solution logic for Day 5 Question2.c

--- DSA/Aarav_590023538/Day_5/Question2.c
+++ DSA/Aarav_590023538/Day_5/Question2.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h>
 
-// Check Equal Arrays
-void bubbleSort(int arr[], int n);
-bool areEqual(int a[], int b[], int n);
+// Function to sort an array using Bubble Sort
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+// Function to check whether two arrays are equal
+bool areEqual(int a[], int b[], int n) {
+    // Sort both arrays
+    bubbleSort(a, n);
+    bubbleSort(b, n);
+    // Compare corresponding elements
+    for (int i = 0; i < n; i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
