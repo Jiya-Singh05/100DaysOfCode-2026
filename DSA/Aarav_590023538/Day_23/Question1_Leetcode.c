51ed8304bc2674963f93876f24b71aa640037e7f
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:56 +0530

    Create initial structure for Day 23 Question1_Leetcode.c

---
+++ DSA/Aarav_590023538/Day_23/Question1_Leetcode.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Assign Cookies (LeetCode #455)
+int compare(const void *a, const void *b);
+int findContentChildren(int* g, int gSize, int* s, int sSize);
