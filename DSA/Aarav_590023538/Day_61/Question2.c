71438ad890231d27f3c737cb4377e0a236ed5d3a
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:20 +0530

    Implement solution logic for Day 61 Question2.c

--- DSA/Aarav_590023538/Day_61/Question2.c
+++ DSA/Aarav_590023538/Day_61/Question2.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-// The Wizard Academy
-int depthOf(int label);
-int minGroups(int n);
+#define MAXN 2001
+
+int mentorArr[MAXN]; // 0-indexed: mentorArr[i] is student (i+1)'s mentor label, or -1
+int memo[MAXN];       // memo[label] = depth of that student in their mentorship chain
+
+int depthOf(int label) {
+    if (memo[label] != 0) return memo[label];
+
+    int m = mentorArr[label - 1];
+    if (m == -1) return memo[label] = 1;
+
+    return memo[label] = 1 + depthOf(m);
+}
+
+int minGroups(int n) {
+    memset(memo, 0, sizeof(memo));
+    int best = 0;
+
+    for (int label = 1; label <= n; label++) {
+        int d = depthOf(label);
+        if (d > best) best = d;
+    }
+    return best;
+}
