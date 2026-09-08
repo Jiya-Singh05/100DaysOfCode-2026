785566dd5d0f599b8df1f3e41a492133066a34e9
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:23 +0530

    Create Question2.c

--- DSA/Aarav_590023538/Day_33/Question2.c
+++ DSA/Aarav_590023538/Day_33/Question2.c
@@ -17,3 +17,36 @@ void push(char str[]) {
 char* pop() {
     return stack[top--];
 }
+
+int main() {
+    char postfix[MAX];
+    printf("Enter postfix expression: ");
+    scanf("%s", postfix);
+    int n = strlen(postfix);
+    for (int i = 0; i < n; i++) {
+        // If current character is an operand, push it as a string
+        if (isalpha(postfix[i])) {
+            char temp[2];
+            temp[0] = postfix[i];
+            temp[1] = '\0';
+            push(temp);
+        }
+        else {
+            // Pop the top two operands
+            char op1[MAX], op2[MAX];
+            strcpy(op2, pop());   // Second operand
+            strcpy(op1, pop());   // First operand
+            // Create prefix expression: operator + first operand + second operand
+            char temp[MAX];
+            temp[0] = postfix[i];
+            temp[1] = '\0';
+            strcat(temp, op1);
+            strcat(temp, op2);
+            // Push the new expression back onto the stack
+            push(temp);
+        }
+    }
+    // Final answer is at the top of the stack
+    printf("Prefix Expression: %s\n", pop());
+    return 0;
+}
