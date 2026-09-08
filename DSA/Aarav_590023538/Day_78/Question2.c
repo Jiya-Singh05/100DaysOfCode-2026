7c1ac787ce7b14a928b47e176b0ce3e9addb2e24
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:53 +0530

    Implement solution logic for Day 78 Question2.c

--- DSA/Aarav_590023538/Day_78/Question2.c
+++ DSA/Aarav_590023538/Day_78/Question2.c
@@ -2,6 +2,57 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 
-// Maximum XOR Pair
-void insert(int num);
-int getMaximumXOR(int num);
+pp
+#include <bits/stdc++.h>
+using namespace std;
+
+class Trie {
+    struct Node {
+        Node* child[2];
+
+        Node() {
+            child[0] = child[1] = nullptr;
+        }
+    };
+
+    Node* root;
+
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(int num) {
+        Node* cur = root;
+
+        for (int bit = 30; bit >= 0; bit--) {
+            int b = (num >> bit) & 1;
+
+            if (!cur->child[b])
+                cur->child[b] = new Node();
+
+            cur = cur->child[b];
+        }
+    }
+
+    int getMaximumXOR(int num) {
+        Node* cur = root;
+        int ans = 0;
+
+        for (int bit = 30; bit >= 0; bit--) {
+            int b = (num >> bit) & 1;
+            int opposite = 1 - b;
+
+            // Choosing the opposite bit makes this XOR bit 1,
+            // which greedily maximizes the answer from MSB to LSB.
+            if (cur->child[opposite]) {
+                ans |= (1 << bit);
+                cur = cur->child[opposite];
+            } else {
+                cur = cur->child[b];
+            }
+        }
+
+        return ans;
+    }
+};
