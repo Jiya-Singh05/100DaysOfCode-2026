5c8a2d65b1e0ed49ad8d3ae84b7b734cfa78a9e4
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:35 +0530

    Implement solution logic for Day 69 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_69/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_69/Question1_Leetcode.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Construct Binary Search Tree from Preorder Traversal (LeetCode #1008)
-struct TreeNode* build(int* preorder, int* idx, int n, int lower, int upper);
-struct TreeNode* bstFromPreorder(int* preorder, int n);
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* build(int* preorder, int* idx, int n, int lower, int upper) {
+    if (*idx == n || preorder[*idx] < lower || preorder[*idx] > upper)
+        return NULL;
+    int val = preorder[*idx];
+    (*idx)++;
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = build(preorder, idx, n, lower, val);
+    node->right = build(preorder, idx, n, val, upper);
+    return node;
+}
+
+struct TreeNode* bstFromPreorder(int* preorder, int n) {
+    int idx = 0;
+    return build(preorder, &idx, n, INT_MIN, INT_MAX);
+}
