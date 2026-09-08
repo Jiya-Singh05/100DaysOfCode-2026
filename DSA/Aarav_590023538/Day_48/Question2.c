0acfb3002b2854ec5839e9d8828792e4071fb49d
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:52 +0530

    Create initial structure for Day 48 Question2.c

---
+++ DSA/Aarav_590023538/Day_48/Question2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Assembly via Minimums
+int cmpDesc(const void *a, const void *b);
+long long* reconstructArray(long long *b, int bn, int n);
+static void printArray(long long *arr, int n);
