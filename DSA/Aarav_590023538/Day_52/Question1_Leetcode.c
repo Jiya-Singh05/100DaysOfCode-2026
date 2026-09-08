01f2634210f22cd253fac07384f79576a3af53f5
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:58 +0530

    Implement solution logic for Day 52 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_52/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_52/Question1_Leetcode.c
@@ -1,7 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 
-// Check if All Characters Have Equal Number of Occurrences
-bool areOccurrencesEqual(char* s);
+// Function to check if all characters occur the same number of times
+bool areOccurrencesEqual(char* s) {
+    // Frequency array for lowercase English letters
+    int freq[26] = {0};
+    int len = strlen(s);
+
+    // Count occurrences of each character
+    for (int i = 0; i < len; i++) {
+        freq[s[i] - 'a']++;
+    }
+
+    // Find the frequency of the first character that appears
+    int target = -1;
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] > 0) {
+            target = freq[i];
+            break;
+        }
+    }
+
+    // Compare every other character's frequency with target
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] > 0 && freq[i] != target) {
+            return false;
+        }
+    }
+
+    return true;
+}
