8f429790a77f90a754e379ce42ba5501c45cb291
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:38 +0530

    Create Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_71/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_71/Question1_Leetcode.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <bits/stdc++.h>
-#include <stdlib.h>
-
 pp
 #include <bits/stdc++.h>
 using namespace std;
@@ -16,4 +12,15 @@ int findJudge(int n, vector<pair<int,int>>& trust) {
         if (score[i] == n - 1) return i;
     }
     return -1;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<pair<int,int>> trust(m);
+    for (int i = 0; i < m; i++) {
+        cin >> trust[i].first >> trust[i].second;
+    }
+    cout << findJudge(n, trust) << "\n";
+    return 0;
 }
