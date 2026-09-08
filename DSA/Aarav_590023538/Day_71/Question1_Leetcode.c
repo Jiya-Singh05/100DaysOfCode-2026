0fdcecf114dcc125b340ed94571d5925746779d5
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:38 +0530

    Implement solution logic for Day 71 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_71/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_71/Question1_Leetcode.c
@@ -2,5 +2,18 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 
-// Find the Town Judge (LeetCode #997)
-int findJudge(int n, vector<pair<int,int>>& trust);
+pp
+#include <bits/stdc++.h>
+using namespace std;
+
+int findJudge(int n, vector<pair<int,int>>& trust) {
+    vector<int> score(n + 1, 0);
+    for (auto& [a, b] : trust) {
+        score[a]--;
+        score[b]++;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (score[i] == n - 1) return i;
+    }
+    return -1;
+}
