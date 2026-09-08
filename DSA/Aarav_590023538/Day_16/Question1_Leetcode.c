1f7dc30cab15284466e579127d8c6006e41bcc0f
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:41 +0530

    Implement solution logic for Day 16 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_16/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_16/Question1_Leetcode.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Swap Nodes In Pairs (LeetCode #24)
-struct ListNode* swapPairs(struct ListNode* head);
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* swapPairs(struct ListNode* head) {
+    // If the list is empty or has only one node, no swapping is needed
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+    // Initialize pointers for adjacent nodes
+    struct ListNode* slow = head;
+    struct ListNode* fast = head->next;
+    // Traverse the list two nodes at a time
+    while (fast != NULL) {
+        // Swap the values of the adjacent nodes
+        int temp = fast->val;
+        fast->val = slow->val;
+        slow->val = temp;
+        // Move slow to the next pair
+        slow = fast->next;
+        // Check if another pair exists
+        if (fast->next == NULL) {
+            fast = NULL;
+        }
+        else {
+            // Move fast to the second node of the next pair
+            fast = fast->next->next;
+        }
+    }
+    // Return the modified linked list
+    return head;
+}
