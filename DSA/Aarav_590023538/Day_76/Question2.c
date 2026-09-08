9a6adb46162edae6d5f1345ec17d0df543272be9
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:50 +0530

    Create initial structure for Day 76 Question2.c

---
+++ DSA/Aarav_590023538/Day_76/Question2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+#include <stdlib.h>
+
+// Does the k-th Line Graph Still Have an Euler Trail?
+bool hasEulerTrail(int n, const vector<pair<int, int>>& edges);
