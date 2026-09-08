86c24abc44646fa4dbd77d865c1644edd3b6cded
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:33 +0530

    Implement solution logic for Day 39 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_39/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_39/Question1_Leetcode.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Time Needed to Buy Tickets (LeetCode #2073)
+python
+# Simple simulation approach - O(n * max_tickets)
+def time_required_to_buy(tickets, k):
+    time = 0
+    i = 0
+    n = len(tickets)
+    while tickets[k] > 0:
+        if tickets[i] > 0:
+            tickets[i] -= 1
+            time += 1
+        i = (i + 1) % n
+    return time
