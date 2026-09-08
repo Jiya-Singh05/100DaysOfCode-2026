466dba262372721627ea2cadb408e4f7c011d66b
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:39:27 +0530

    Implement solution logic for Day 80 Question2.c

--- DSA/Sahaj_590026086/Day_80/Question2.c
+++ DSA/Sahaj_590026086/Day_80/Question2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
-// Power of Two or Zero?
-bool isPowerOfTwoOrZero(int n);
+bool isPowerOfTwoOrZero(int n) {
+    return n == 0 || (n > 0 && (n & (n - 1)) == 0);
+}
