c71e0ca218b5ab3639981ce8f98cb97fdfb17988
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:28 +0530

    Implement solution logic for Day 10 Question2.c

--- DSA/Aarav_590023538/Day_10/Question2.c
+++ DSA/Aarav_590023538/Day_10/Question2.c
@@ -3,6 +3,14 @@
 
 // Determine Gender
 int main() {
-    // Initial structure
+char username[101];
+    int freq[26] = {0}; // To store whether a character appeared or not
+    int i, distinct = 0;
+    // Input username
+    scanf("%s", username);
+    // Traverse the string
+    for (i = 0; username[i] != '\0'; i++) {
+        // If character is seen for the first time
+        if (freq[username[i] - 'a'] == 0) {
     return 0;
 }
