0e02b943fad6b246a0df2ad055fd3d90ae6953e0
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:38:11 +0530

    Create initial structure for Day 74 Question1_leetcode.c

---
+++ DSA/Sahaj_590026086/Day_74/Question1_leetcode.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// LeetCode 2316: Count Unreachable Pairs of Nodes in an Undirected Graph
+long long countPairs(int n, int** edges, int edgesSize, int* edgesColSize);
