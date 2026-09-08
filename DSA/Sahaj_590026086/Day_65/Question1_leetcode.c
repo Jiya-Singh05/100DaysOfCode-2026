2be2d0b21d7b8278c1066d61fa3d32d08e799621
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:37:50 +0530

    Implement solution logic for Day 65 Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_65/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_65/Question1_leetcode.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// LeetCode 501: Find Mode in Binary Search Tree
 struct TreeNode { int val; struct TreeNode *left; struct TreeNode *right; };
-int* findMode(struct TreeNode* root, int* returnSize);
+void inorder(struct TreeNode* root, int* prev, int* count, int* maxCount, int* modes, int* modeCount, int pass) {
+    if (!root) return;
+    inorder(root->left, prev, count, maxCount, modes, modeCount, pass);
+    if (*prev == root->val) (*count)++; else { *count = 1; *prev = root->val; }
+    if (pass == 1) { if (*count > *maxCount) *maxCount = *count; }
+    else { if (*count == *maxCount) modes[(*modeCount)++] = root->val; }
+    inorder(root->right, prev, count, maxCount, modes, modeCount, pass);
+}
