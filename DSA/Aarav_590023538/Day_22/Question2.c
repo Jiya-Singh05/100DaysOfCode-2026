300adbf7ee4719a51230ab5116ac24c0e4881360
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:55 +0530

    Implement solution logic for Day 22 Question2.c

--- DSA/Aarav_590023538/Day_22/Question2.c
+++ DSA/Aarav_590023538/Day_22/Question2.c
@@ -3,6 +3,27 @@
 
 // Card Picking Game
 int main() {
-    // Initial structure
+int n;
+    // Input number of cards
+    scanf("%d", &n);
+    int cards[n];
+    // Input card values
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &cards[i]);
+    }
+    int left = 0;
+    int right = n - 1;
+    int player1 = 0;
+    int player2 = 0;
+    int turn = 0; // 0 -> Player 1, 1 -> Player 2
+    // Continue until all cards are picked
+    while (left <= right)
+    {
+        int picked;
+        // Pick the larger value from either end
+        if (cards[left] > cards[right])
+        {
+            picked = cards[left];
     return 0;
 }
