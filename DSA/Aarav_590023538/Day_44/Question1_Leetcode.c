d9181f69bca5d9600246f989fd6df39983ea76fb
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:42 +0530

    Implement solution logic for Day 44 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_44/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_44/Question1_Leetcode.c
@@ -2,5 +2,15 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 
-// Can Make Arithmetic Progression From Sequence
-bool canMakeArithmeticProgression(vector<int>& arr);
+pp
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canMakeArithmeticProgression(vector<int>& arr) {
+    sort(arr.begin(), arr.end());
+    int diff = arr[1] - arr[0];
+    for (int i = 2; i < arr.size(); i++) {
+        if (arr[i] - arr[i - 1] != diff) return false;
+    }
+    return true;
+}
