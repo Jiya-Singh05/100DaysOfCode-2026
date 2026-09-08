6618ddd85b274832ea5362e9bcee94e08c4cee3a
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:39:36 +0530

    Implement solution logic for Day 83 Question2.c

--- DSA/Sahaj_590026086/Day_83/Question2.c
+++ DSA/Sahaj_590026086/Day_83/Question2.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// Count Set Bits
-int countSetBits(int n);
+int countSetBits(int n) {
+    int count = 0;
+    while (n) {
+        count += n & 1;
+        n >>= 1;
+    }
+    return count;
+}
