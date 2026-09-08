743c8fb9f658784ef845669b1df0458ccce6b03f
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:56 +0530

    Create Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_51/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_51/Question1_Leetcode.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <bits/stdc++.h>
-#include <stdlib.h>
-
 pp
 #include <bits/stdc++.h>
 using namespace std;
@@ -20,4 +16,17 @@ int mostFrequentEven(vector<int>& nums) {
         }
     }
     return best;
+}
+
+int main() {
+    vector<int> n1 = {0,1,2,2,4,4,1};
+    cout << mostFrequentEven(n1) << endl; // 2
+
+    vector<int> n2 = {4,4,4,9,2,4};
+    cout << mostFrequentEven(n2) << endl; // 4
+
+    vector<int> n3 = {29,47,21,41,13,37,25,7};
+    cout << mostFrequentEven(n3) << endl; // -1
+
+    return 0;
 }
