bca3d2372016059cd7f155cf7f4c436d38433e34
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:30 +0530

    Implement solution logic for Day 38 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_38/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_38/Question1_Leetcode.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Number of Students Unable to Eat Lunch
-public int countStudents(int[] students, int[] sandwiches);
+java
+import java.util.*;
+
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int[] count = new int[2]; // count[0] = students wanting circular, count[1] = students wanting square
+        for (int s : students) {
+            count[s]++;
+        }
+        
+        int i = 0; // pointer into sandwiches stack
+        while (i < sandwiches.length) {
+            int type = sandwiches[i];
+            if (count[type] == 0) {
+                // No student left wants this sandwich type -> rest are stuck
+                break;
+            }
+            count[type]--;
+            i++;
+        }
+        
+        return count[0] + count[1];
+    }
+}
