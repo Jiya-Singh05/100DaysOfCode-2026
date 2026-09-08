92cd9acd6065efd8bc66d8fce8ce58961b97778b
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:41 +0530

    Implement solution logic for Day 73 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_73/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_73/Question1_Leetcode.c
@@ -2,5 +2,35 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 
-// Find Closest Node to Given Two Nodes (LeetCode #2359)
-int closestMeetingNode(vector<int>& edges, int node1, int node2);
+pp
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> bfsDist(int start, vector<int>& edges) {
+    int n = edges.size();
+    vector<int> dist(n, -1);
+    dist[start] = 0;
+    int cur = start;
+    while (edges[cur] != -1 && dist[edges[cur]] == -1) {
+        dist[edges[cur]] = dist[cur] + 1;
+        cur = edges[cur];
+    }
+    return dist;
+}
+
+int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+    vector<int> dist1 = bfsDist(node1, edges);
+    vector<int> dist2 = bfsDist(node2, edges);
+
+    int n = edges.size();
+    int best = -1, bestDist = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        if (dist1[i] == -1 || dist2[i] == -1) continue;
+        int d = max(dist1[i], dist2[i]);
+        if (d < bestDist) {
+            bestDist = d;
+            best = i;
+        }
+    }
+    return best;
+}
