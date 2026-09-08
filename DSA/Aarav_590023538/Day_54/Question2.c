478030f9fc08939347b0db3a7d925aede3361ff6
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:05 +0530

    Implement solution logic for Day 54 Question2.c

--- DSA/Aarav_590023538/Day_54/Question2.c
+++ DSA/Aarav_590023538/Day_54/Question2.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-// Football Championship Winner
-const char* footballChampion(char goals[][11], int n);
+const char* footballChampion(char goals[][11], int n) {
+    static char team1[11], team2[11];
+    int count1 = 0, count2 = 0;
+    int haveTeam2 = 0;
+
+    strcpy(team1, goals[0]);
+
+    for (int i = 0; i < n; i++) {
+        if (strcmp(goals[i], team1) == 0) {
+            count1++;
+        } else if (!haveTeam2) {
+            strcpy(team2, goals[i]);
+            haveTeam2 = 1;
+            count2++;
+        } else if (strcmp(goals[i], team2) == 0) {
+            count2++;
+        }
+    }
+
+    return (count1 > count2) ? team1 : team2;
+}
