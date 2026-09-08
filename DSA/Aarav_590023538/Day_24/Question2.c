22416c434e0de093dcdef00469fe1775f3868858
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:00 +0530

    Implement solution logic for Day 24 Question2.c

--- DSA/Aarav_590023538/Day_24/Question2.c
+++ DSA/Aarav_590023538/Day_24/Question2.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Squares of a Sorted Array
-void sortedSquares(int nums[], int n, int result[]);
+// Function to return the sorted squares of the array
+void sortedSquares(int nums[], int n, int result[]) {
+    int left = 0;
+    int right = n - 1;
+    int index = n - 1;
+
+    // Compare absolute values from both ends
+    while (left <= right) {
+        int leftSquare = nums[left] * nums[left];
+        int rightSquare = nums[right] * nums[right];
+
+        // Place the larger square at the end of the result array
+        if (leftSquare > rightSquare) {
+            result[index] = leftSquare;
+            left++;
+        } else {
+            result[index] = rightSquare;
+            right--;
+        }
+        index--;
+    }
+}
