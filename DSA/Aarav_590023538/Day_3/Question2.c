5d82c1d6c59b6bb6a478649290b6d4cd876a9907
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:12 +0530

    Create Question2.c

--- DSA/Aarav_590023538/Day_3/Question2.c
+++ DSA/Aarav_590023538/Day_3/Question2.c
@@ -17,3 +17,26 @@ int countOccurrences(int arr[], int n, int target)
     // Return the total count
     return count;
 }
+
+int main()
+{
+    int n, target;
+    // Read the size of the array
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    // Read the array elements
+    printf("Enter %d sorted elements:\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    // Read the target element
+    printf("Enter the target element: ");
+    scanf("%d", &target);
+    // Call the function to count occurrences
+    int result = countOccurrences(arr, n, target);
+    // Display the result
+    printf("Number of occurrences of %d = %d\n", target, result);
+    return 0;
+}
