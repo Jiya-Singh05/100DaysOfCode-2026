4cfb531a6dc5a110027b734c93ddc7c7d4a595c0
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:39 +0530

    Implement solution logic for Day 15 Question2.c

--- DSA/Aarav_590023538/Day_15/Question2.c
+++ DSA/Aarav_590023538/Day_15/Question2.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Reverse a Singly Linked List
-struct Node* reverseList(struct Node *head);
-void printList(struct Node *head);
+// Structure of a linked list node
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+// Function to reverse the linked list
+struct Node* reverseList(struct Node *head) {
+    struct Node *prev = NULL;
+    struct Node *curr = head;
+    struct Node *next = NULL;
+
+    // Reverse the links
+    while (curr != NULL) {
+        // Store the next node
+        next = curr->next;
+        // Reverse the current node's link
+        curr->next = prev;
+        // Move all pointers one step forward
+        prev = curr;
+        curr = next;
+    }
+
+    // prev becomes the new head
+    return prev;
+}
+
+// Function to print the linked list
+void printList(struct Node *head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+// Driver code
