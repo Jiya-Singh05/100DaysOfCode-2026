cabcffb47e46d1ad8d802487888207f1e96c31d5
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:38:02 +0530

    Implement solution logic for Day 69 Question2.c

--- DSA/Sahaj_590026086/Day_69/Question2.c
+++ DSA/Sahaj_590026086/Day_69/Question2.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// The Great Divide
-int canPartition(int* nums, int n);
+int canPartition(int* nums, int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) sum += nums[i];
+    return sum % 2 == 0;
+}
