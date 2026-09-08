17d19a2cbb453dfe14964f24b11bbb276543884a
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:44 +0530

    Implement solution logic for Day 74 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_74/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_74/Question1_Leetcode.c
@@ -2,7 +2,41 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 
-// Count Unreachable Pairs of Nodes in an Undirected Graph (LeetCode #2316)
-int find(int x);
-void unite(int a, int b);
-long long countPairs(int n, vector<vector<int>>& edges);
+pp
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> parent, sz;
+
+int find(int x) {
+    while (parent[x] != x) {
+        parent[x] = parent[parent[x]];
+        x = parent[x];
+    }
+    return x;
+}
+
+void unite(int a, int b) {
+    a = find(a); b = find(b);
+    if (a == b) return;
+    if (sz[a] < sz[b]) swap(a, b);
+    parent[b] = a;
+    sz[a] += sz[b];
+}
+
+long long countPairs(int n, vector<vector<int>>& edges) {
+    parent.resize(n);
+    sz.assign(n, 1);
+    iota(parent.begin(), parent.end(), 0);
+
+    for (auto& e : edges) unite(e[0], e[1]);
+
+    long long remaining = n, ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (find(i) == i) { // representative of a component
+            ans += (long long) sz[i] * (remaining - sz[i]);
+            remaining -= sz[i];
+        }
+    }
+    return ans;
+}
