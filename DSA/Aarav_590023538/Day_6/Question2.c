da9635da8e486abb4b79f6b80f1f8d848d7f9d78
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:20 +0530

    Implement solution logic for Day 6 Question2.c

--- DSA/Aarav_590023538/Day_6/Question2.c
+++ DSA/Aarav_590023538/Day_6/Question2.c
@@ -3,6 +3,32 @@
 
 // Union of Two Sorted Arrays
 int main() {
-    // Initial structure
+int a[100], b[100], c[200];
+    int n, m;
+    int i, j, k = 0, found;
+    // Input size of first array
+    scanf("%d", &n);
+    // Input elements of first array
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    // Input size of second array
+    scanf("%d", &m);
+    // Input elements of second array
+    for (i = 0; i < m; i++)
+        scanf("%d", &b[i]);
+    // Add unique elements from the first array to the union array
+    for (i = 0; i < n; i++) {
+        found = 0;
+        // Check if the element already exists in the union array
+        for (j = 0; j < k; j++) {
+            if (a[i] == c[j]) {
+                found = 1;
+                break;
+            }
+        }
+        // If not found, add it to the union array
+        if (!found)
+            c[k++] = a[i];
+    }
     return 0;
 }
