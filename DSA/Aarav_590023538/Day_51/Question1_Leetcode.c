4099b3f86950f8423d3cf0666c185d03262e8974
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:56 +0530

    Implement solution logic for Day 51 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_51/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_51/Question1_Leetcode.c
@@ -2,5 +2,22 @@
 #include <bits/stdc++.h>
 #include <stdlib.h>
 
-// Most Frequent Even Element (LeetCode #2404)
-int mostFrequentEven(vector<int>& nums);
+pp
+#include <bits/stdc++.h>
+using namespace std;
+
+int mostFrequentEven(vector<int>& nums) {
+    unordered_map<int, int> freq;
+    for (int x : nums) {
+        if (x % 2 == 0) freq[x]++;
+    }
+
+    int best = -1, bestCount = 0;
+    for (auto& [val, cnt] : freq) {
+        if (cnt > bestCount || (cnt == bestCount && val < best)) {
+            best = val;
+            bestCount = cnt;
+        }
+    }
+    return best;
+}
