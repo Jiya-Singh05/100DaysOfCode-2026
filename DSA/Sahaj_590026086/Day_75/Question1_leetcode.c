79145707b5ad5739abdf4057a22433282ebe64f3
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:38:15 +0530

    Implement solution logic for Day 75 Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_75/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_75/Question1_leetcode.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-// LeetCode 1857: Largest Color Value in a Directed Graph
-int largestPathValue(char * colors, int** edges, int edgesSize, int* edgesColSize);
+// Topological sort and dynamic programming for colors
