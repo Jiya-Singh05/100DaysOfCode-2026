aeb22ec21138f626a9c79ef91f6b8d46da8e18ca
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:47 +0530

    Implement solution logic for Day 18 Question2.c

--- DSA/Aarav_590023538/Day_18/Question2.c
+++ DSA/Aarav_590023538/Day_18/Question2.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Remove Nth Node From End of Linked List
-struct Node* removeNthFromEnd(struct Node *head, int n);
-void printList(struct Node *head);
+// Structure of a linked list node
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+// Function to remove the nth node from the end
+struct Node* removeNthFromEnd(struct Node *head, int n) {
+    // Create a temporary node to handle deletion of the head node
+    struct Node *tempHead = (struct Node *)malloc(sizeof(struct Node));
+    tempHead->next = head;
+    struct Node *fast = tempHead;
+    struct Node *slow = tempHead;
+    // Move the fast pointer n+1 steps ahead
+    for (int i = 0; i <= n; i++) {
+        fast = fast->next;
+    }
+    // Move both pointers until fast reaches the end
+    while (fast != NULL) {
+        fast = fast->next;
+        slow = slow->next;
+    }
+    // Delete the required node
+    struct Node *temp = slow->next;
+    slow->next = temp->next;
+    free(temp);
+    head = tempHead->next;
+    free(tempHead);
+    return head;
+}
+
+// Function to print the linked list
+void printList(struct Node *head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
