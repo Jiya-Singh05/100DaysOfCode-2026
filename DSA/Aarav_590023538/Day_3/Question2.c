965da9d3163b23c64fb60b73f656e4c59c02c580
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:12 +0530

    Implement solution logic for Day 3 Question2.c

--- DSA/Aarav_590023538/Day_3/Question2.c
+++ DSA/Aarav_590023538/Day_3/Question2.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-// Number of Occurrences
-int countOccurrences(int arr[], int n, int target);
+// Function to count the occurrences of the target element
+int countOccurrences(int arr[], int n, int target)
+{
+    int count = 0;
+    // Traverse the entire array
+    for (int i = 0; i < n; i++)
+    {
+        // If the current element matches the target,
+        // increase the count
+        if (arr[i] == target)
+        {
+            count++;
+        }
+    }
+    // Return the total count
+    return count;
+}
