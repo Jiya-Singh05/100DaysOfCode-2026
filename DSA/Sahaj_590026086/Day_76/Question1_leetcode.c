5ac605f77a94119dc08319c5d30f25b1c94f43d5
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:38:18 +0530

    Implement solution logic for Day 76 Question1_leetcode.c

--- DSA/Sahaj_590026086/Day_76/Question1_leetcode.c
+++ DSA/Sahaj_590026086/Day_76/Question1_leetcode.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// LeetCode 1916: Count Ways to Build Rooms in an Ant Colony
-int waysToBuildRooms(int* prevRoom, int prevRoomSize);
+#define MOD 1000000007
+long long power(long long base, long long exp) {
+    long long res = 1; base %= MOD;
+    while (exp > 0) {
+        if (exp % 2 == 1) res = (res * base) % MOD;
+        base = (base * base) % MOD; exp /= 2;
+    }
+    return res;
+}
