82d5cef34761997991d8f5e4d5b4fe13bdb0018d
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:33 +0530

    Implement solution logic for Day 39 Question2.c

--- DSA/Aarav_590023538/Day_39/Question2.c
+++ DSA/Aarav_590023538/Day_39/Question2.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Minimum Conference Rooms Required
+python
+# Optimized O(n) approach
+def time_required_to_buy(tickets, k):
+    time = 0
+    for i, t in enumerate(tickets):
+        if i <= k:
+            time += min(t, tickets[k])
+        else:
+            time += min(t, tickets[k] - 1)
+    return time
