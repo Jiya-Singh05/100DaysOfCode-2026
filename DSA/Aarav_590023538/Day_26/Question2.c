c06d3f451fb9c549ad35fecef8ca67d79553f772
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:07 +0530

    Create Question2.c

--- DSA/Aarav_590023538/Day_26/Question2.c
+++ DSA/Aarav_590023538/Day_26/Question2.c
@@ -39,3 +39,27 @@ bool checkTriplet(int arr[], int n) {
     // No valid triplet exists
     return false;
 }
+
+int main() {
+    int n;
+
+    // Input the size of the array
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input array elements
+    printf("Enter the array elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Check and display the result
+    if (checkTriplet(arr, n))
+        printf("true\n");
+    else
+        printf("false\n");
+
+    return 0;
+}
