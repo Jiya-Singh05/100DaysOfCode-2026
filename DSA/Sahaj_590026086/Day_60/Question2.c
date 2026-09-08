28788a371e3fd4a8f6bdb5d2772a010ec5f71ae2
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:37:41 +0530

    Create Question2.c

--- DSA/Sahaj_590026086/Day_60/Question2.c
+++ DSA/Sahaj_590026086/Day_60/Question2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int maxSubarraySum(int* arr, int n) {
     int max_so_far = arr[0], curr_max = arr[0];
     for (int i = 1; i < n; i++) {
@@ -7,4 +8,10 @@ int maxSubarraySum(int* arr, int n) {
         if (curr_max > max_so_far) max_so_far = curr_max;
     }
     return max_so_far;
+}
+
+int main() {
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    printf("%d\n", maxSubarraySum(arr, 9));
+    return 0;
 }
