c0c47217a2f5bc75299bb07cb5d8df0a6a5ca222
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:19 +0530

    Implement solution logic for Day 61 Question1_Leetcode.c

--- DSA/Aarav_590023538/Day_61/Question1_Leetcode.c
+++ DSA/Aarav_590023538/Day_61/Question1_Leetcode.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Find the Town Judge (LeetCode #997)
-int findJudge(int n, int trust[][2], int trustSize);
+int findJudge(int n, int trust[][2], int trustSize) {
+    int *score = calloc(n + 1, sizeof(int));
+
+    for (int i = 0; i < trustSize; i++) {
+        int a = trust[i][0], b = trust[i][1];
+        score[a]--; // a trusts someone -> not a candidate judge
+        score[b]++; // b is trusted by someone
+    }
+
+    int judge = -1;
+    for (int i = 1; i <= n; i++) {
+        if (score[i] == n - 1) { judge = i; break; }
+    }
+
+    free(score);
+    return judge;
+}
