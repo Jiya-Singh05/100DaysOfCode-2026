3783e5c984592e9f095e3db08684af005abe69d4
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:14 +0530

    Implement solution logic for Day 4 Question2.c

--- DSA/Aarav_590023538/Day_4/Question2.c
+++ DSA/Aarav_590023538/Day_4/Question2.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-// Rotate Array by K Steps
-void rotateArray(int arr[], int n, int k);
+// Function to rotate the array to the right by k steps
+void rotateArray(int arr[], int n, int k)
+{
+    // If k is greater than array size, reduce unnecessary rotations
+    k = k % n;
+    // Rotate the array one step at a time
+    for (int i = 0; i < k; i++)
+    {
+        // Store the last element
+        int last = arr[n - 1];
+        // Shift all elements one position to the right
+        for (int j = n - 1; j > 0; j--)
+        {
+            arr[j] = arr[j - 1];
+        }
+        // Place the last element at the beginning
+        arr[0] = last;
+    }
+}
