98a78e1bdcb3032974f9fc92c0adf32c943b54e8
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:48 +0530

    Implement solution logic for Day 75 Question2.c

--- DSA/Aarav_590023538/Day_75/Question2.c
+++ DSA/Aarav_590023538/Day_75/Question2.c
@@ -4,6 +4,19 @@
 
 // Minimum Cost to Make a Ring Strongly Connected
 int main() {
-    // Initial structure
+int n;
+    cin >> n;
+    // adj[u] holds the (at most 2) roads touching u, each stored as
+    // {neighbor, sign, cost} where sign = +1 if the road is given as u->v,
+    // and sign = -1 if the road is given as v->u.
+    vector<vector<array<int, 3>>> adj(n + 1);
+    long long totalCost = 0;
+    for (int i = 0; i < n; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        adj[a].push_back({b, 1, c});
+        adj[b].push_back({a, -1, c});
+        totalCost += c;
+    }
     return 0;
 }
