0b11b3d27ef025bcd7df3e15693bd1d14e4ff407
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:28:51 +0530

    Create Question2.c

--- DSA/Aarav_590023538/Day_76/Question2.c
+++ DSA/Aarav_590023538/Day_76/Question2.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <bits/stdc++.h>
-#include <stdlib.h>
-
 pp
 #include <bits/stdc++.h>
 using namespace std;
@@ -19,4 +15,43 @@ bool hasEulerTrail(int n, const vector<pair<int, int>>& edges) {
         odd += (degree[i] & 1);
 
     return odd == 0 || odd == 2;
+}
+
+int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    vector<pair<int, int>> edges(m);
+    for (auto& [u, v] : edges)
+        cin >> u >> v;
+
+    vector<pair<int, int>> curEdges = edges;
+    int curN = n;
+
+    for (int step = 0; step < k; step++) {
+        vector<vector<int>> incident(curN + 1);
+
+        for (int i = 0; i < (int)curEdges.size(); i++) {
+            auto [u, v] = curEdges[i];
+            incident[u].push_back(i);
+            incident[v].push_back(i);
+        }
+
+        vector<pair<int, int>> nextEdges;
+
+        for (int u = 1; u <= curN; u++) {
+            auto& list = incident[u];
+            for (int i = 0; i < (int)list.size(); i++) {
+                for (int j = i + 1; j < (int)list.size(); j++) {
+                    nextEdges.push_back({list[i] + 1, list[j] + 1});
+                }
+            }
+        }
+
+        curN = curEdges.size();
+        curEdges.swap(nextEdges);
+    }
+
+    cout << (hasEulerTrail(curN, curEdges) ? "true\n" : "false\n");
+    return 0;
 }
