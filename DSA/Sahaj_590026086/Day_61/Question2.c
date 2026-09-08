759357b545c433d33b7105d97ed788ba03c4960f
Sahaj Parikh <sahajparikh2007@gmail.com>
2026-09-08 11:37:43 +0530

    Implement solution logic for Day 61 Question2.c

--- DSA/Sahaj_590026086/Day_61/Question2.c
+++ DSA/Sahaj_590026086/Day_61/Question2.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// The Wizard Academy
-int wizardMaster(int* powers, int n);
+int wizardMaster(int* powers, int n) {
+    int maxP = powers[0];
+    for (int i = 1; i < n; i++) if (powers[i] > maxP) maxP = powers[i];
+    return maxP;
+}
