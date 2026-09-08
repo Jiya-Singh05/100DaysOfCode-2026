82476ab83cd75e663dce18f547a20e3d01fc13e6
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:26:13 +0530

    Create initial structure for Day 4 Question1_Leetcode.c

---
+++ DSA/Aarav_590023538/Day_4/Question1_Leetcode.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Transpose Matrix (LeetCode #867)
+int** transpose(int** matrix, int matrixSize, int* matrixColSize, int* returnSize, int** returnColumnSizes);
