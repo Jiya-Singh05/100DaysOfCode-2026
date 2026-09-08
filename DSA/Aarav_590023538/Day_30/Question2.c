5cd66eff090123dcb061e4608069253fb379e44d
Aarav Saxena <2006aarav1226@gmail.com>
2026-09-08 12:27:15 +0530

    Implement solution logic for Day 30 Question2.c

--- DSA/Aarav_590023538/Day_30/Question2.c
+++ DSA/Aarav_590023538/Day_30/Question2.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define MAX 100
 
-// Insert Element at Bottom of Stack
-void push(int value);
-int pop();
-void insertAtBottom(int x);
-void display();
+int stack[MAX];
+int top = -1;
+
+// Push an element onto the stack
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    stack[++top] = value;
+}
+
+// Pop the top element from the stack
+int pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return -1;
+    }
+    return stack[top--];
+}
+
+// Insert an element at the bottom of the stack
+void insertAtBottom(int x) {
+    // If stack is empty, insert the element
+    if (top == -1) {
+        push(x);
+        return;
+    }
+    // Remove the top element
+    int temp = pop();
+    // Recursively insert x at the bottom
+    insertAtBottom(x);
+    // Push the removed element back
+    push(temp);
+}
+
+// Display the stack from bottom to top
+void display() {
+    printf("Updated Stack: ");
+    for (int i = 0; i <= top; i++) {
+        printf("%d ", stack[i]);
+    }
+    printf("\n");
+}
